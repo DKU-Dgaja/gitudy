04eb53c2f53c39c40206817827c72c2c14f73004
j-ra1n <j_ra1n@naver.com>
2024-01-16 00:34:06 +0900

    [BE] test(#30): Google Adapter Test
    * 토큰 요청 테스트
    * 프로필 요청 테스트

---
+++ backend/src/test/java/com/example/backend/auth/api/service/oauth/adapter/google/GoogleAdapterTest.java
@@ -0,0 +1,119 @@
+package com.example.backend.auth.api.service.oauth.adapter.google;
+
+
+import com.example.backend.auth.TestConfig;
+import com.example.backend.auth.api.service.oauth.builder.google.GoogleURLBuilder;
+import com.example.backend.auth.api.service.oauth.response.OAuthResponse;
+import com.example.backend.common.exception.ExceptionMessage;
+import com.example.backend.common.exception.oauth.OAuthException;
+
+import com.example.backend.external.clients.oauth.google.GoogleProfileClients;
+import com.example.backend.external.clients.oauth.google.GoogleTokenClients;
+import com.example.backend.external.clients.oauth.google.response.GoogleProfileResponse;
+import com.example.backend.external.clients.oauth.google.response.GoogleTokenResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.net.URI;
+
+import static com.example.backend.domain.define.user.constant.UserPlatformType.GOOGLE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class GoogleAdapterTest extends TestConfig {
+
+    @Autowired
+    private GoogleAdapter googleAdapter;
+
+    @Autowired
+    private GoogleURLBuilder googleURLBuilder;
+
+    @Test
+    @DisplayName("google 토큰 요청 API에 정상적인 요청을 보내면, access_token이 발행된다.")
+    void googleAdapterGetTokenSuccess() {
+        // given
+        GoogleAdapterTest.MockGoogleTokenClients mockGoogleTokenClients = new GoogleAdapterTest.MockGoogleTokenClients();
+        GoogleAdapterTest.MockGoogleProfileClients mockGoogleProfileClients = new GoogleAdapterTest.MockGoogleProfileClients();
+        GoogleAdapter googleAdapter = new GoogleAdapter(mockGoogleTokenClients, mockGoogleProfileClients);
+
+        // when
+        String accessToken = googleAdapter.getToken("tokenUrl");
+
+        // then
+        System.out.println("accessToken = " + accessToken);
+        assertThat(accessToken).isEqualTo("access-token");
+
+    }
+
+    @Test
+    @DisplayName("google 토큰 요청 중 예외가 발생하면, OAUTH_INVALID_TOKEN_URL 에외가 발생한다.")
+    void googleAdapterGetTokenFail() {
+        // given
+        String tokenURL = googleURLBuilder.token("error-token", "state");
+
+        // when
+        OAuthException exception = assertThrows(OAuthException.class,
+                () -> googleAdapter.getToken(tokenURL));
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo(ExceptionMessage.OAUTH_INVALID_TOKEN_URL.getText());
+
+    }
+
+    @Test
+    @DisplayName("google 프로필 요청 API에 정상적인 요청을 보내면, 사용자 프로필이 반환된다.")
+    void googleAdapterGetProfileSuccess() {
+        // given
+        GoogleAdapterTest.MockGoogleTokenClients mockGoogleTokenClients = new GoogleAdapterTest.MockGoogleTokenClients();
+        GoogleAdapterTest.MockGoogleProfileClients mockGoogleProfileClients = new GoogleAdapterTest.MockGoogleProfileClients();
+        GoogleAdapter googleAdapter = new GoogleAdapter(mockGoogleTokenClients, mockGoogleProfileClients);
+
+        // when
+        OAuthResponse profile = googleAdapter.getProfile("access-token");
+
+        // then
+        assertAll(
+                () -> assertThat(profile.getPlatformId()).isEqualTo("102514823309503386675"),  // google은 sub
+                () -> assertThat(profile.getEmail()).isEqualTo("xw21yog@dankook.ac.kr"),
+                () -> assertThat(profile.getProfileImageUrl()).isEqualTo("https://lh3.googleusercontent.com/a/ACg8ocLrP_GLo-fUjSmnUZedPZbbL7ifImYTnelh108XkgOx=s96-c"),
+                () -> assertThat(profile.getName()).isEqualTo("이정우"),
+                () -> assertThat(profile.getPlatformType()).isEqualTo(GOOGLE)
+        );
+    }
+
+    @Test
+    @DisplayName("google 프로필 요청 중 예외가 발생하면, OAUTH_INVALID_ACCESS_TOKEN 예외가 발생한다.")
+    void googleAdapterGetProfileFail() {
+
+        // when
+        OAuthException exception = assertThrows(OAuthException.class,
+                () -> googleAdapter.getProfile("error-token"));
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo(ExceptionMessage.OAUTH_INVALID_ACCESS_TOKEN.getText());
+
+    }
+
+
+
+    static class MockGoogleTokenClients implements GoogleTokenClients {
+
+        @Override
+        public GoogleTokenResponse getToken(URI uri) {
+            return new GoogleTokenResponse("access-token");
+        }
+    }
+
+    static class MockGoogleProfileClients implements GoogleProfileClients {
+
+        @Override
+        public GoogleProfileResponse getProfile(String header) {
+            return new GoogleProfileResponse("102514823309503386675",
+                    "이정우",
+                    "xw21yog@dankook.ac.kr",
+                    "https://lh3.googleusercontent.com/a/ACg8ocLrP_GLo-fUjSmnUZedPZbbL7ifImYTnelh108XkgOx=s96-c");
+        }
+    }
+}
