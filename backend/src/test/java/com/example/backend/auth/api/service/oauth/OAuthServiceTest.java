ff90cd9a3f084b19349fe7857280066eae26c8b0
kooyeongmin <rn9dud0@naver.com>
2024-01-16 17:56:04 +0900

    [BE] test(#29): OAuth Service Test
    * 모든 플랫폼 로그인 페이지 반환 test에 카카오 추가
    * 카카오 로그인 성공, 실패 테스트 추가

--- backend/src/test/java/com/example/backend/auth/api/service/oauth/OAuthServiceTest.java
+++ backend/src/test/java/com/example/backend/auth/api/service/oauth/OAuthServiceTest.java
@@ -3,7 +3,9 @@ package com.example.backend.auth.api.service.oauth;
 import com.example.backend.auth.TestConfig;
 import com.example.backend.auth.api.controller.auth.response.AuthLoginPageResponse;
 import com.example.backend.auth.api.service.oauth.adapter.github.GithubAdapter;
+import com.example.backend.auth.api.service.oauth.adapter.kakao.KakaoAdapter;
 import com.example.backend.auth.api.service.oauth.builder.github.GithubURLBuilder;
+import com.example.backend.auth.api.service.oauth.builder.kakao.KakaoURLBuilder;
 import com.example.backend.auth.api.service.oauth.response.OAuthResponse;
 import com.example.backend.common.exception.oauth.OAuthException;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +16,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.List;
 
 import static com.example.backend.domain.define.user.constant.UserPlatformType.GITHUB;
+import static com.example.backend.domain.define.user.constant.UserPlatformType.KAKAO;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -29,6 +32,10 @@ class OAuthServiceTest extends TestConfig {
     @MockBean
     private GithubAdapter githubAdapter;
 
+    @Autowired
+    private KakaoURLBuilder kakaoUrlBuilder;
+    @MockBean
+    private KakaoAdapter kakaoAdapter;
     @Test
     @DisplayName("모든 플랫폼의 로그인 페이지를 성공적으로 반환한다.")
     void allUrlBuilderSuccess() {
@@ -38,9 +45,20 @@ class OAuthServiceTest extends TestConfig {
         // when
         List<AuthLoginPageResponse> loginPages = oAuthService.loginPage(state);
         String authorizeURL = urlBuilder.authorize(state);
-
+        String authorizeURLkakao = kakaoUrlBuilder.authorize(state);
         // then
-        assertThat(loginPages.get(0).getUrl()).isEqualTo(authorizeURL);
+        assertThat(loginPages).hasSize(2); // 리스트 크기 확인
+
+        // 각 플랫폼별 URL인지 확인
+        boolean containsGithub = loginPages.stream()
+                .anyMatch(page -> page.getPlatformType().equals(GITHUB) &&
+                        page.getUrl().equals(authorizeURL));
+        boolean containsKakao = loginPages.stream()
+                .anyMatch(page -> page.getPlatformType().equals(KAKAO) &&
+                        page.getUrl().equals(authorizeURLkakao));
+
+        assertThat(containsGithub).isTrue();
+        assertThat(containsKakao).isTrue();
     }
 
     @Test
@@ -73,7 +91,35 @@ class OAuthServiceTest extends TestConfig {
     }
 
     @Test
-    @DisplayName("깃허브 로그인에 실하면 OAuthException 예외가 발생한다.")
+    @DisplayName("카카오 로그인에 성공하면 OAuthResponse 객체를 반환한다.")
+    void kakaoLoginSuccess() {
+        // given
+        String code = "valid-code";
+        String state = "valid-state";
+
+        OAuthResponse response = OAuthResponse.builder()
+                .platformId("1")
+                .platformType(KAKAO)
+                .email("이메일 없음")
+                .name("구영민")
+                .profileImageUrl("http://k.kakaocdn.net/dn/1G9kp/btsAot8liOn/8CWudi3uy07rvFNUkk3ER0/img_640x640.jpg")
+                .build();
+
+        // when
+        // when 사용시 Mockito 패키지 사용
+        when(kakaoAdapter.getToken(any(String.class))).thenReturn("access-token");
+        when(kakaoAdapter.getProfile(any(String.class))).thenReturn(response);
+        OAuthResponse profile = oAuthService.login(KAKAO, code, state);
+
+        // then
+        assertThat(profile)
+                .extracting("platformId", "platformType", "email", "name", "profileImageUrl")
+                .contains("1", KAKAO, "이메일 없음", "구영민", "http://k.kakaocdn.net/dn/1G9kp/btsAot8liOn/8CWudi3uy07rvFNUkk3ER0/img_640x640.jpg");
+
+
+    }
+    @Test
+    @DisplayName("깃허브 로그인에 실패하면 OAuthException 예외가 발생한다.")
     void githubLoginFail() {
         // given
         String code = "invalid-code";
@@ -83,6 +129,20 @@ class OAuthServiceTest extends TestConfig {
         when(githubAdapter.getToken(any(String.class))).thenThrow(OAuthException.class);
         assertThrows(OAuthException.class,
                 () -> oAuthService.login(GITHUB, code, state));
+
+    }
+
+    @Test
+    @DisplayName("카카오 로그인에 실패하면 OAuthException 예외가 발생한다.")
+    void kakaoLoginFail() {
+        // given
+        String code = "invalid-code";
+        String state = "invalid-state";
+
+        // when
+        when(kakaoAdapter.getToken(any(String.class))).thenThrow(OAuthException.class);
+        assertThrows(OAuthException.class,
+                () -> oAuthService.login(KAKAO, code, state));
 
     }
 }
