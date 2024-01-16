7dcead9be62e1ab03cc4779056773da8394b49ae
kooyeongmin <rn9dud0@naver.com>
2024-01-16 17:46:03 +0900

    [BE] feat(#29): Kakao Profile Clients, Response 구현
    * 프로필 uri로 요청 구현
    * 프로필을 요청 후 반환받을 DTO 구현
    * 카카오에선 email을 제공하지 않음
    * response 받는 형식을 다르게 함

---
+++ backend/src/main/java/com/example/backend/external/clients/oauth/kakao/response/KakaoProfileResponse.java
@@ -0,0 +1,35 @@
+package com.example.backend.external.clients.oauth.kakao.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class KakaoProfileResponse {
+    private Long id;
+    private String name;
+    private String email;
+
+    private Properties properties;
+
+    public KakaoProfileResponse(Long id, String name, String email, Properties properties) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.properties = properties;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Properties {
+        private String nickname;
+        private String profile_image;
+        private String thumbnail_image;
+
+        public Properties(String nickname, String profile_image, String thumbnail_image) {
+            this.nickname = nickname;
+            this.profile_image = profile_image;
+            this.thumbnail_image = thumbnail_image;
+        }
+    }
+}
