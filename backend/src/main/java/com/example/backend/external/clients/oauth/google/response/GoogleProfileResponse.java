614e2d9dc9429d1001b9bbba271a1f7b6de14cd2
j-ra1n <j_ra1n@naver.com>
2024-01-16 10:32:39 +0900

    [BE] feat(#30): Google Profile Clients, Response 구현
    * 프로필 uri로 요청 구현
    * 프로필을 요청 후 반환받을 DTO 구현

---
+++ backend/src/main/java/com/example/backend/external/clients/oauth/google/response/GoogleProfileResponse.java
@@ -0,0 +1,26 @@
+package com.example.backend.external.clients.oauth.google.response;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+/*
+    Google에 사용자 정보를 요청 후 반환받을 DTO
+ */
+@Getter
+@NoArgsConstructor    // 사용자 프로필 응답 데이터
+public class GoogleProfileResponse {
+    private String sub;  // Google에서 사용하는 사용자의 고유 식별자 (subject)
+    private String name;
+    private String email;
+    private String picture;
+
+
+    public GoogleProfileResponse(String sub, String name, String email, String picture) {
+        this.sub = sub;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+}
