e484fffedcfe6dc38529eb5ba526f01b08146a32
j-ra1n <j_ra1n@naver.com>
2024-01-21 13:23:07 +0900

    [BE] refactor(#46): state 수정
    * redis 재정의
    *CRUD 작업을 위한 인터페이스 재정의

--- backend/src/main/java/com/example/backend/domain/define/state/LoginState.java
+++ backend/src/main/java/com/example/backend/domain/define/state/LoginState.java
@@ -1,16 +1,16 @@
 package com.example.backend.domain.define.state;
 
-import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.annotation.Id;
 
 import java.util.UUID;
 
 @Getter
 @ToString
-@RedisHash(value = "state", timeToLive = 60 * 10) // 10분
+@RedisHash(value = "state", timeToLive = 60 * 3) // 3분
 public class LoginState {
 
     @Id
@@ -20,6 +20,7 @@ public class LoginState {
 
     @Builder
     private LoginState(boolean isUse) {
+        this.state=UUID.randomUUID();
         this.isUse = isUse;
     }
 }
