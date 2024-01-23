c8ee6588fd767f918fdabf8d0e4a11909330a397
이주성 <32183520@dankook.ac.kr>
2024-01-23 10:51:15 +0900

    [BE] refactor(#63): RedisTest 도메인 & 테스트 삭제

--- backend/src/main/java/com/example/backend/domain/define/test/repository/RedisTestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.domain.define.test.repository;
-
-import com.example.backend.domain.define.test.RedisTest;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RedisTestRepository extends CrudRepository<RedisTest, Long> {
-}
