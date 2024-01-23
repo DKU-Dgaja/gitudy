d8eddf12602872c2c38d4b5283977152e506eb82
이주성 <32183520@dankook.ac.kr>
2024-01-23 15:16:26 +0900

    [BE] feat(#63) Entity 클래스 정의

---
+++ backend/src/main/java/com/example/backend/domain/define/study/category/mapping/StudyCategoryMapping.java
@@ -0,0 +1,6 @@
+package com.example.backend.domain.define.study.category.mapping;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+
+public class StudyCategoryMapping extends Serializers.Base {
+}
