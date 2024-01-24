b9dee530d16c37a40e20b86084e11abb5dbc05b8
j-ra1n <j_ra1n@naver.com>
2024-01-24 09:43:02 +0900

    [BE] refactor(#46): LoginState 피드백
    *피드백 마지막 반영

--- backend/src/test/java/com/example/backend/external/clients/oauth/google/GoogleClientTest.java
+++ backend/src/test/java/com/example/backend/external/clients/oauth/google/GoogleClientTest.java
@@ -1,4 +1,3 @@
-/*
 package com.example.backend.external.clients.oauth.google;
 
 import com.example.backend.auth.TestConfig;
@@ -22,25 +21,24 @@ public class GoogleClientTest extends TestConfig {
     @DisplayName("인가 code를 URL에 담아 요청해 access_token을 성공적으로 받환받는다.")
     void googleTokenRequestTest() {
         // given
-        String uri = "https://oauth2.googleapis.com/token?grant_type=authorization_code&client_id=990624479058-n15n90hki5la1cf8246nmg2nchu7qofn.apps.googleusercontent.com&client_secret=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&redirect_uri=http://localhost:8080/auth/GOOGLE/login&code=";
+       // String uri = "https://oauth2.googleapis.com/token?grant_type=authorization_code&client_id=990624479058-n15n90hki5la1cf8246nmg2nchu7qofn.apps.googleusercontent.com&client_secret=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&redirect_uri=http://localhost:8080/auth/GOOGLE/login&code=";
 
         // when
-        GoogleTokenResponse token = googleTokenClients.getToken(URI.create(uri));
-        System.out.println("token = " + token.getAccess_token());
+       //   GoogleTokenResponse token = googleTokenClients.getToken(URI.create(uri));
+       // System.out.println("token = " + token.getAccess_token());
 
         // then
         // 현재는 동적인 테스트 불가하므로 null. OauthService 구현 후 동적으로 테스트할 예정
-        assertNull(token.getAccess_token());
+       // assertNull(token.getAccess_token());
     }
 
     @Test
     @DisplayName("Access Token을 URL에 담아 요청해 사용자 정보를 성공적으로 받환받는다.")
     void googleProfileRequestTest() {
         // given
-        String accessToken = "";
 
-*/
-/*        GoogleProfileResponse profile = googleProfileClients.getProfile("Bearer " + accessToken);
+
+/*      GoogleProfileResponse profile = googleProfileClients.getProfile("Bearer " + accessToken);
 
         System.out.println("login = " + profile.getLogin());
         System.out.println("email = " + profile.getEmail());
@@ -50,8 +48,8 @@ public class GoogleClientTest extends TestConfig {
                 () -> assertThat(profile.getLogin()).isEqualTo("j-ra1n"),
                 () -> assertThat(profile.getEmail()).isEqualTo("xw21yog@dankook.ac.kr"),
                 () -> assertThat(profile.getName()).isEqualTo("이정우")
-        );*//*
+        );*/
 
     }
 }
-*/
+
