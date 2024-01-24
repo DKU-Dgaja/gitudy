0c04b3a8a5675194435f247c4612cc4ef8800b46
j-ra1n <j_ra1n@naver.com>
2024-01-24 11:39:04 +0900

    [BE] refactor(#68): 오류수정

--- backend/src/main/java/com/example/backend/auth/api/service/state/LoginStateService.java
+++ backend/src/main/java/com/example/backend/auth/api/service/state/LoginStateService.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
 
 @Slf4j
 @Service
@@ -25,30 +24,21 @@ public class LoginStateService {
         LoginState savedLoginState = loginStateRepository.save(loginState);
         log.info(">>>> [ Login State 생성 ]  {}", savedLoginState);
 
-        return savedLoginState.getState().toString();
+        return savedLoginState.getState();
     }
 
     // Login State 검증
     public boolean isValidLoginState(String loginState) {
-        UUID uuid;
-
-        // UUID 형식이 아닐 경우 예외처리
-        try {
-            uuid = UUID.fromString(loginState);
-        } catch (IllegalArgumentException e) {
-            log.warn(">>>> {} : {}", loginState, ExceptionMessage.LOGINSTATE_INVALID_VALUE);
-            throw new LoginStateException(ExceptionMessage.LOGINSTATE_INVALID_VALUE);
-        }
 
         // 객체를 Redis에서 조회후 없는 경우 예외 처리
-        LoginState findLoginState = loginStateRepository.findById(toString())
+        LoginState findLoginState = loginStateRepository.findById(loginState)
                 .orElseThrow(() -> {
                     log.warn(">>>> {} : {}", loginState, ExceptionMessage.LOGINSTATE_NOT_FOUND);
                     throw new LoginStateException(ExceptionMessage.LOGINSTATE_NOT_FOUND);
                 });
 
         // 사용한 LoginState는 삭제
-        loginStateRepository.deleteById(toString());
+        loginStateRepository.deleteById(loginState);
 
         return true;
     }
