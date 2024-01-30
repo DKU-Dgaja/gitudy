f67926a84dc7f248ce06d78f076ab8a456d7e797
탁세하 <takseha98@gmail.com>
2024-01-30 21:50:27 +0900

    [FE] feat(#72): 마이 커밋 더보기 화면 UI 구현

---
+++ frontend/presentation/src/main/java/com/takseha/presentation/ui/profile/MoreCommitActivity.kt
@@ -0,0 +1,12 @@
+package com.takseha.presentation.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.takseha.presentation.R
+
+class MoreCommitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more_commit)
+    }
+}
