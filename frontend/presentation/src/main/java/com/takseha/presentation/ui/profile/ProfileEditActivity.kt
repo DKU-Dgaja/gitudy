913e727d4c751a671f001fa3600e6e853e77c0af
탁세하 <takseha98@gmail.com>
2024-01-30 15:52:33 +0900

    [FE] feat(#72): 내 정보 수정하기 UI 구현
    switch 관련 drawable 생성

---
+++ frontend/presentation/src/main/java/com/takseha/presentation/ui/profile/ProfileEditActivity.kt
@@ -0,0 +1,12 @@
+package com.takseha.presentation.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.takseha.presentation.R
+
+class ProfileEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_edit)
+    }
+}
