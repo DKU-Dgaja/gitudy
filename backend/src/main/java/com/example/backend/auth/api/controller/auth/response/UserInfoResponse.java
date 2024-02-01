f0382cf0b8e088393f2a37ca445bb18f8ed7ceaa
이주성 <32183520@dankook.ac.kr>
2024-02-01 22:50:26 +0900

    [BE] refactor(#91): User 테이블 프로필 공개 여부 필드 추가

--- backend/src/main/java/com/example/backend/auth/api/controller/auth/response/UserInfoResponse.java
+++ backend/src/main/java/com/example/backend/auth/api/controller/auth/response/UserInfoResponse.java
@@ -7,44 +7,36 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class UserInfoResponse {
-
-    private Long id;
-    private String platformId;
-    private UserPlatformType platformType;
     private UserRole role;
     private String githubId;
     private String name;
     private String profileImageUrl;
-    private boolean pushAlarmYn ;
+    private boolean pushAlarmYn;
+    private boolean profilePublicYn;
     private int score;
     private int point;
 
-
-    private void User(Long id, String platformId, UserPlatformType platformType, UserRole role, String githubId, String name, String profileImageUrl, boolean pushAlarmYn, int score, int point) {
-        this.id = id;
-        this.platformId = platformId;
-        this.platformType = platformType;
+    @Builder
+    public UserInfoResponse(UserRole role, String githubId, String name, String profileImageUrl, boolean pushAlarmYn, boolean profilePublicYn, int score, int point) {
         this.role = role;
         this.githubId = githubId;
         this.name = name;
         this.profileImageUrl = profileImageUrl;
         this.pushAlarmYn = pushAlarmYn;
+        this.profilePublicYn = profilePublicYn;
         this.score = score;
         this.point = point;
     }
 
     public static UserInfoResponse of(User user){
         return UserInfoResponse.builder()
-                .id(user.getId())
-                .platformId(user.getPlatformId())
-                .platformType(user.getPlatformType())
                 .role(user.getRole())
                 .githubId(user.getGithubId())
                 .name(user.getName())
                 .profileImageUrl(user.getProfileImageUrl())
                 .pushAlarmYn(user.isPushAlarmYn())
+                .profilePublicYn(user.isProfilePublicYn())
                 .score(user.getScore())
                 .point(user.getPoint())
                 .build();
