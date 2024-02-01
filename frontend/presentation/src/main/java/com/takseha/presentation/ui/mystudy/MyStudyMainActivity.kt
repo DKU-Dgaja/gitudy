63cc3c4ed8adfd4b02f3126323c5571562dbc413
탁세하 <takseha98@gmail.com>
2024-02-01 17:26:40 +0900

    [AN] feat(#86): 스터디 상세 페이지(스터디 홈)

---
+++ frontend/presentation/src/main/java/com/takseha/presentation/ui/mystudy/MyStudyMainActivity.kt
@@ -0,0 +1,12 @@
+package com.takseha.presentation.ui.mystudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.takseha.presentation.R
+
+class MyStudyMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_study_main)
+    }
+}
