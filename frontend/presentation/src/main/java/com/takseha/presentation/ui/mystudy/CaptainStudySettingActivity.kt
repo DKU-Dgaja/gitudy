400b5e14f6bffe709dca5ece2cfbd92d45946ea4
탁세하 <takseha98@gmail.com>
2024-02-02 04:31:45 +0900

    [AN] feat(#86): 스터디 설정(팀장) 페이지 UI 구현

---
+++ frontend/presentation/src/main/java/com/takseha/presentation/ui/mystudy/CaptainStudySettingActivity.kt
@@ -0,0 +1,12 @@
+package com.takseha.presentation.ui.mystudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.takseha.presentation.R
+
+class CaptainStudySettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_captain_study_setting)
+    }
+}
