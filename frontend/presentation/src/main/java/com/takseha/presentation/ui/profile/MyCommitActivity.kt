3a124e5a5259431af5c8554e8effab173e82e03e
탁세하 <takseha98@gmail.com>
2024-02-02 01:00:53 +0900

    [AN] feat(#86): 키보드 사용 시 확인 버튼 떠오르게 하는 기능 구현

--- frontend/presentation/src/main/java/com/takseha/presentation/ui/profile/MyCommitActivity.kt
+++ frontend/presentation/src/main/java/com/takseha/presentation/ui/profile/MyCommitActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.takseha.presentation.R
 
-class MoreCommitActivity : AppCompatActivity() {
+class MyCommitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_commit)
