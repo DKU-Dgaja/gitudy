5f63e53605bb4b3651dd7fc2e713c57865329b12
kooyeongmin <rn9dud0@naver.com>
2024-02-04 19:30:20 +0900

    [BE] feat(#93): @JsonProperty 추가
    * Java객체와 JSON데이터가 매핑이 잘 안돼서 @JsonProperty추가하였습니다.

--- backend/src/main/java/com/example/backend/domain/define/study/info/StudyInfo.java
+++ backend/src/main/java/com/example/backend/domain/define/study/info/StudyInfo.java
@@ -4,6 +4,7 @@ import com.example.backend.domain.define.BaseEntity;
 import com.example.backend.domain.define.study.info.constant.RepositoryInfo;
 import com.example.backend.domain.define.study.info.constant.StudyPeriodType;
 import com.example.backend.domain.define.study.info.constant.StudyStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -24,49 +25,63 @@ public class StudyInfo extends BaseEntity {
     @Column(name = "STUDY_INFO_ID")
     private Long id;                                // 아이디
 
+    @JsonProperty("userId")
     @Column(name = "USER_ID", nullable = false)
     private Long userId;                            // 스터디장 ID
 
+    @JsonProperty("topic")
     @Column(name = "TOPIC", nullable = false)
     private String topic;                           // 스터디 이름
 
+    @JsonProperty("score")
     @Column(name = "SCORE")
     private int score = 0;                          // 스터디 활동점수
 
+    @JsonProperty("endDate")
     @Temporal(TemporalType.DATE)
     @Column(name = "END_DATE")
     private LocalDate endDate;                      // 스터디 종료일
 
+    @JsonProperty("info")
     @Column(name = "INFO", length = 512)
     private String info;                            // 스터디 소개
 
+    @JsonProperty("studyStatus")
     @Enumerated(EnumType.STRING)
     @Column(name = "STUDY_STATUS")
     @ColumnDefault(value = "'STUDY_PUBLIC'")
     private StudyStatus status;                     // 스터디 상태
 
+    @JsonProperty("joinCode")
     @Column(name = "JOIN_CODE")
     private String joinCode;                        // 스터디 참여 코드
 
+    @JsonProperty("maximumMember")
     @Column(name = "MAXIMUM_MEMBER")
     private int maximumMember;                      // 스터디 제한 인원
 
+    @JsonProperty("currentMember")
     @Column(name = "CURRENT_MEMBER")
     private int currentMember;                      // 스터디 현재 인원
 
+    @JsonProperty("lastCommitDay")
     @Temporal(TemporalType.DATE)
     @Column(name = "LAST_COMMIT_DAY")
     private LocalDate lastCommitDay;                // 스터디 마지막 활동 시간
 
+    @JsonProperty("profileImageURL")
     @Column(name = "PROFILE_IMAGE_URL")
     private String profileImageUrl;                 // 스터디 프로필 사진
 
+    @JsonProperty("notice")
     @Column(name = "NOTICE")
     private String notice;                          // 스터디 공지
 
+    @JsonProperty("repositoryInfo")
     @Embedded
     private RepositoryInfo repositoryInfo;          // 연동할 깃허브 레포지토리 정보
 
+    @JsonProperty("studyPeriodNone")
     @Enumerated(EnumType.STRING)
     @Column(name = "STUDY_PREIOD_TYPE")
     @ColumnDefault(value = "'STUDY_PERIOD_NONE'")
