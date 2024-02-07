728e0616381f2a5541e5432120eeedbb388d0080
탁세하 <takseha98@gmail.com>
2024-02-07 22:26:12 +0900

    [AN] feat(#103): 스터디 신청 페이지 UI 구현

---
+++ frontend/presentation/src/main/java/com/takseha/presentation/ui/feed/StudyApplyActivity.kt
@@ -0,0 +1,12 @@
+package com.takseha.presentation.ui.feed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.takseha.presentation.R
+
+class StudyApplyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_study_apply)
+    }
+}
