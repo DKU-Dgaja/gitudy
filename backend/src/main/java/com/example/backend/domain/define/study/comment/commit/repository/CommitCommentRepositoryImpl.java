c44c36bf73dbe236e6dec69eb67a5fbba06d39ea
이주성 <32183520@dankook.ac.kr>
2024-02-07 12:32:32 +0900

    [BE] feat(#108): 사용자 정보가 포함된 커밋 댓글 리스트 조회 쿼리 구현

---
+++ backend/src/main/java/com/example/backend/domain/define/study/comment/commit/repository/CommitCommentRepositoryImpl.java
@@ -0,0 +1,48 @@
+package com.example.backend.domain.define.study.comment.commit.repository;
+
+import com.example.backend.auth.api.controller.auth.response.UserInfoResponse;
+import com.example.backend.study.api.controller.comment.commit.response.CommitCommentInfoResponse;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.example.backend.domain.define.account.user.QUser.user;
+import static com.example.backend.domain.define.study.comment.commit.QCommitComment.commitComment;
+
+@Component
+@RequiredArgsConstructor
+public class CommitCommentRepositoryImpl implements CommitCommentRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<CommitCommentInfoResponse> findCommitCommentListByCommitIdJoinUser(Long commitId) {
+
+        return jpaQueryFactory.select(Projections.constructor(CommitCommentInfoResponse.class,
+                        commitComment.id,
+                        commitComment.studyCommitId,
+                        commitComment.userId,
+                        commitComment.content,
+                        commitComment.createdDateTime,
+                        commitComment.modifiedDateTime,
+                        Projections.constructor(UserInfoResponse.class,
+                                user.id,
+                                user.role,
+                                user.githubId,
+                                user.name,
+                                user.profileImageUrl,
+                                user.pushAlarmYn,
+                                user.profilePublicYn,
+                                user.score,
+                                user.point
+                        )
+                ))
+                .from(commitComment)
+                .join(user).on(user.id.eq(commitComment.userId))
+                .where(commitComment.studyCommitId.eq(commitId))
+                .orderBy(commitComment.id.asc())
+                .fetch();
+    }
+}
---
+++ backend/src/test/java/com/example/backend/domain/define/study/comment/commit/repository/CommitCommentRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.example.backend.domain.define.study.comment.commit.repository;
+
+import com.example.backend.auth.TestConfig;
+import com.example.backend.auth.config.fixture.UserFixture;
+import com.example.backend.domain.define.account.user.User;
+import com.example.backend.domain.define.account.user.repository.UserRepository;
+import com.example.backend.domain.define.study.comment.commit.CommitCommentFixture;
+import com.example.backend.study.api.controller.comment.commit.response.CommitCommentInfoResponse;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SuppressWarnings("NonAsciiCharacters")
+class CommitCommentRepositoryTest extends TestConfig {
+    @Autowired
+    private CommitCommentRepository commitCommentRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @AfterEach
+    void tearDown() {
+//        commitCommentRepository.deleteAllInBatch();
+//        userRepository.deleteAllInBatch();
+    }
+
+    @Test
+    void 사용자_정보가_포함된_커밋_댓글_리스트_조회_성공_테스트() {
+        // given
+        Long commitId = 1L;
+        Long otherCommitId = 2L;
+
+        User userA = userRepository.save(User.builder().platformId("A").profileImageUrl("testA").build());
+        User userB = userRepository.save(User.builder().platformId("B").profileImageUrl("testB").build());
+        User userC = userRepository.save(User.builder().platformId("C").profileImageUrl("testC").build());
+
+        commitCommentRepository.saveAll(CommitCommentFixture.createDefaultCommitCommentList(5, userA.getId(), commitId));
+        commitCommentRepository.saveAll(CommitCommentFixture.createDefaultCommitCommentList(5, userB.getId(), otherCommitId));
+        commitCommentRepository.saveAll(CommitCommentFixture.createDefaultCommitCommentList(5, userC.getId(), commitId));
+        commitCommentRepository.saveAll(CommitCommentFixture.createDefaultCommitCommentList(5, userC.getId(), otherCommitId));
+
+        // when
+        List<CommitCommentInfoResponse> response = commitCommentRepository.findCommitCommentListByCommitIdJoinUser(commitId);
+
+        // then
+        for (CommitCommentInfoResponse c : response) {
+//            System.out.println("c.getId() = " + c.getId());
+//            System.out.println("c.getStudyCommitId() = " + c.getStudyCommitId());
+//            System.out.println("c.getUserInfoResponse().getUserId() = " + c.getUserInfoResponse().getUserId());
+            assertEquals(commitId, c.getStudyCommitId());
+            assertNotNull(c.getUserInfoResponse().getUserId());
+        }
+    }
+
+}
