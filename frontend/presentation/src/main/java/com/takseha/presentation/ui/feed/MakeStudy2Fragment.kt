2493d7f6098a3ef266fa569803660770e8abd20d
탁세하 <takseha98@gmail.com>
2024-02-08 16:01:21 +0900

    [AN] feat(#103): 스터디 개설 페이지(2) UI 구현

--- frontend/presentation/src/main/java/com/takseha/presentation/ui/feed/MakeStudy2Fragment.kt
+++ frontend/presentation/src/main/java/com/takseha/presentation/ui/feed/MakeStudy2Fragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MakeStudyFragment.newInstance] factory method to
+ * Use the [MakeStudy2Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MakeStudyFragment : Fragment() {
+class MakeStudy2Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class MakeStudyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_make_study, container, false)
+        return inflater.inflate(R.layout.fragment_make_study2, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class MakeStudyFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MakeStudyFragment.
+         * @return A new instance of fragment MakeStudy2Fragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MakeStudyFragment().apply {
+            MakeStudy2Fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
