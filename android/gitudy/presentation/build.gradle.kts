febfcb18700bbb42c8bf96522830bcae1ab1a763
탁세하 <takseha98@gmail.com>
2024-02-11 15:32:57 +0900

    [AN] refactor(#111): login 관련 nav 추가

--- android/gitudy/presentation/build.gradle.kts
+++ android/gitudy/presentation/build.gradle.kts
@@ -35,14 +35,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
+    viewBinding {
+        enable = true
+    }
     dataBinding {
         enable = true
     }
-    buildFeatures {
-        viewBinding = true
-    }
-
 }
 
 dependencies {
@@ -56,6 +54,8 @@ dependencies {
     implementation("androidx.annotation:annotation:1.7.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/LoginActivity.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/LoginActivity.kt
@@ -6,18 +6,29 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.takseha.presentation.R
+import com.takseha.presentation.databinding.ActivityLoginBinding
 import com.takseha.presentation.ui.mystudy.CommentActivity
 import com.takseha.presentation.ui.mystudy.MyStudyMainActivity
 import com.takseha.presentation.ui.profile.ProfileEditActivity
 import com.takseha.presentation.ui.profile.ProfileHomeFragment
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val b = findViewById<LinearLayout>(R.id.githubLoginBtn)
-        b.setOnClickListener {
-            startActivity(Intent(this, CommentActivity::class.java))
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.githubLoginBtn.setOnClickListener {
+
         }
+        binding.otherLoginBtn.setOnClickListener {
+            startActivity(Intent(this, SubLoginActivity::class.java))
+        }
+
     }
 }
