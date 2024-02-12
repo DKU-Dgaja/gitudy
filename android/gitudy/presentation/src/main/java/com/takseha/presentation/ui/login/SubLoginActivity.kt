520fed5240462ddaadf14aa9edddf5850209aca9
탁세하 <takseha98@gmail.com>
2024-02-12 23:16:20 +0900

    [AN] feat(#112): SubLoginActivity 내 viewbinding 구현

--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/SubLoginActivity.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/SubLoginActivity.kt
@@ -3,10 +3,24 @@ package com.takseha.presentation.ui.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.takseha.presentation.R
+import com.takseha.presentation.databinding.ActivitySubLoginBinding
 
 class SubLoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySubLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub_login)
+
+        binding = ActivitySubLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.kakaoLoginBtn.setOnClickListener {
+
+        }
+        binding.googleLoginBtn.setOnClickListener {
+
+        }
     }
 }
