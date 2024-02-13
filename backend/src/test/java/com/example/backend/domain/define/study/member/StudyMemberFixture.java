2fb52a09123aef6a795c6e428644a5fc4b97bdb0
이주성 <32183520@dankook.ac.kr>
2024-02-13 16:05:33 +0900

    [BE] test(#118): 활동중인 스터디원인지 판단하는 쿼리 테스트

---
+++ backend/src/test/java/com/example/backend/domain/define/study/member/StudyMemberFixture.java
@@ -0,0 +1,38 @@
+package com.example.backend.domain.define.study.member;
+
+import com.example.backend.domain.define.study.member.constant.StudyMemberRole;
+import com.example.backend.domain.define.study.member.constant.StudyMemberStatus;
+
+public class StudyMemberFixture {
+    // 테스트용 스터디장 생성 메서드
+    public static StudyMember createStudyMemberLeader(Long userId, Long studyInfoId) {
+        return StudyMember.builder()
+                .userId(userId)
+                .studyInfoId(studyInfoId)
+                .role(StudyMemberRole.STUDY_LEADER)
+                .status(StudyMemberStatus.STUDY_ACTIVE)
+                .build();
+    }
+
+    // 테스트용 활동중인 스터디원 생성 메서드
+    public static StudyMember createDefaultStudyMember(Long userId, Long studyInfoId) {
+        return StudyMember.builder()
+                .userId(userId)
+                .studyInfoId(studyInfoId)
+                .role(StudyMemberRole.STUDY_MEMBER)
+                .status(StudyMemberStatus.STUDY_ACTIVE)
+                .build();
+    }
+
+    // 테스트용 강퇴당한 스터디원 생성 메서드
+    public static StudyMember createStudyMemberResigned(Long userId, Long studyInfoId) {
+        return StudyMember.builder()
+                .userId(userId)
+                .studyInfoId(studyInfoId)
+                .role(StudyMemberRole.STUDY_MEMBER)
+                .status(StudyMemberStatus.STUDY_RESIGNED)
+                .build();
+    }
+
+
+}
---
+++ backend/src/test/java/com/example/backend/domain/define/study/member/repository/StudyMemberRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.example.backend.domain.define.study.member.repository;
+
+import com.example.backend.auth.TestConfig;
+import com.example.backend.domain.define.study.member.StudyMember;
+import com.example.backend.domain.define.study.member.StudyMemberFixture;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SuppressWarnings("NonAsciiCharacters")
+class StudyMemberRepositoryTest extends TestConfig {
+    @Autowired
+    private StudyMemberRepository studyMemberRepository;
+
+    @AfterEach
+    void tearDown() {
+        studyMemberRepository.deleteAllInBatch();
+    }
+
+    @Test
+    void 해당_유저가_스터디의_활동중인_스터디원인지_확인_스터디원일_경우() {
+        // given
+        Long userId = 1L;
+        Long studyInfoId = 1L;
+
+        StudyMember savedMember = studyMemberRepository.save(StudyMemberFixture.createDefaultStudyMember(userId, studyInfoId));
+
+        // when
+        assertTrue(studyMemberRepository.existsStudyMemberByUserIdAndStudyInfoId(savedMember.getUserId(), savedMember.getStudyInfoId()));
+    }
+
+    @Test
+    void 해당_유저가_스터디의_활동중인_스터디원인지_확인_테스트_스터디원이_아닐_경우() {
+        // given
+        Long userId = 1L;
+        Long studyInfoId = 1L;
+
+        StudyMember savedMember = studyMemberRepository.save(StudyMemberFixture.createStudyMemberResigned(userId, studyInfoId));
+
+        // when
+        assertFalse(studyMemberRepository.existsStudyMemberByUserIdAndStudyInfoId(savedMember.getUserId(), savedMember.getStudyInfoId()));
+    }
+
+}
