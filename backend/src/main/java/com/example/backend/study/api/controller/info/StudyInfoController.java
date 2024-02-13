9d0ccd4d3fb2954e2674925d654534e585d606ff
kooyeongmin <rn9dud0@naver.com>
2024-02-13 11:32:50 +0900

    [BE] feat(#114) 스터디 등록 컨트롤러, 서비스 구현

---
+++ backend/src/main/java/com/example/backend/study/api/controller/info/StudyInfoController.java
@@ -0,0 +1,33 @@
+package com.example.backend.study.api.controller.info;
+
+import com.example.backend.auth.api.service.auth.AuthService;
+import com.example.backend.common.response.JsonResult;
+import com.example.backend.domain.define.account.user.User;
+import com.example.backend.study.api.controller.info.request.StudyInfoRegisterRequest;
+import com.example.backend.study.api.controller.info.response.StudyInfoRegisterResponse;
+import com.example.backend.study.api.service.info.StudyInfoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/studyinfo")
+public class StudyInfoController {
+    private final StudyInfoService studyInfoService;
+    private final AuthService authService;
+
+    @PostMapping("/")
+    public JsonResult<?> registerStudy(@AuthenticationPrincipal User user,
+                                       @Valid @RequestBody StudyInfoRegisterRequest studyInfoRequest) {
+        authService.authenticate(user.getId(), user);
+        StudyInfoRegisterResponse response = studyInfoService.registerStudy(studyInfoRequest);
+        return JsonResult.successOf("Study Register Success.");
+    }
+}
---
+++ backend/src/main/java/com/example/backend/study/api/service/info/StudyInfoService.java
@@ -0,0 +1,39 @@
+package com.example.backend.study.api.service.info;
+
+import com.example.backend.domain.define.study.info.StudyInfo;
+import com.example.backend.domain.define.study.info.repository.StudyInfoRepository;
+import com.example.backend.study.api.controller.info.request.StudyInfoRegisterRequest;
+import com.example.backend.study.api.controller.info.response.StudyInfoRegisterResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StudyInfoService {
+    private final StudyInfoRepository studyInfoRepository;
+    @Transactional
+    public StudyInfoRegisterResponse registerStudy(StudyInfoRegisterRequest request) {
+        StudyInfo studyInfo = StudyInfo.builder()
+                .userId(request.getUserId())
+                .topic(request.getTopic())
+                .score(0)
+                .endDate(request.getEndDate())
+                .info(request.getInfo())
+                .status(request.getStatus())
+                .joinCode(request.getJoinCode())
+                .maximumMember(request.getMaximumMember())
+                .currentMember(1)
+                .lastCommitDay(null)
+                .profileImageUrl(request.getProfileImageUrl())
+                .notice(null)
+                .repositoryInfo(request.getRepositoryInfo())
+                .periodType(request.getPeriodType())
+                .build();
+        studyInfoRepository.save(studyInfo);
+        return StudyInfoRegisterResponse.of(studyInfo);
+    }
+}
