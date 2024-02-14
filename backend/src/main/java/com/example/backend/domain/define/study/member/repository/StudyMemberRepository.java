e13ed20cffe9036d2119a5024018a7e3688ec2b7
kooyeongmin <rn9dud0@naver.com>
2024-02-14 18:41:41 +0900

    [BE] feat(#114): 충돌 해결

--- backend/src/main/java/com/example/backend/domain/define/study/member/repository/StudyMemberRepository.java
+++ backend/src/main/java/com/example/backend/domain/define/study/member/repository/StudyMemberRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StudyMemberRepository extends JpaRepository<StudyMember, Long> {
-    List<StudyMember> findByStudyInfoId(Long studyInfoId);
+    
 }
