9f9cb54a94915f632e5e9166c12ab62905ed391a
j-ra1n <j_ra1n@naver.com>
2024-02-14 21:21:34 +0900

    [BE] refactor(#117): 피드백반영

--- backend/src/main/java/com/example/backend/domain/define/study/member/repository/StudyMemberRepository.java
+++ backend/src/main/java/com/example/backend/domain/define/study/member/repository/StudyMemberRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface StudyMemberRepository extends JpaRepository<StudyMember, Long> {
+public interface StudyMemberRepository  extends JpaRepository<StudyMember, Long>, StudyMemberRepositoryCustom {
     List<StudyMember> findByStudyInfoId(Long studyInfoId);
 }
