4015f835510be11b6d620ac068103d924df9698a
탁세하 <takseha98@gmail.com>
2024-02-20 00:07:31 +0900

    [AN] feat(#112): login webview 페이지 생성

--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/SubLoginActivity.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/SubLoginActivity.kt
@@ -1,16 +1,20 @@
 package com.takseha.presentation.ui.login
 
-import android.content.Intent
+import android.content.ComponentName
+import android.content.Context
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsClient
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsServiceConnection
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.takseha.presentation.R
 import com.takseha.presentation.databinding.ActivitySubLoginBinding
 import com.takseha.presentation.viewmodel.LoginViewModel
+
 
 class SubLoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySubLoginBinding
@@ -24,13 +28,8 @@ class SubLoginActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
 
         binding.kakaoLoginBtn.setOnClickListener {
-            viewModel.startLogin("KAKAO")
-            viewModel.loginPageUrl.observe(this, Observer {
-                Log.d("LoginViewModel", "loginPageUrl: $it")
-                val intentKakaoLogin: CustomTabsIntent = CustomTabsIntent.Builder()
-                    .build()
-                intentKakaoLogin.launchUrl(this, Uri.parse(it))
-            })
+            startLogin("KAKAO")
+
         }
         binding.googleLoginBtn.setOnClickListener {
 
@@ -41,5 +40,15 @@ class SubLoginActivity : AppCompatActivity() {
         binding = ActivitySubLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
+
+    private fun startLogin(platformType: String) {
+        viewModel.startLogin(platformType)
+        viewModel.loginPageUrl.observe(this, Observer {
+            Log.d("LoginViewModel", "loginPageUrl: $it")
+//            val loginIntent: CustomTabsIntent = CustomTabsIntent.Builder()
+//                .build()
+//            loginIntent.launchUrl(this, Uri.parse(it))
+        })
     }
 }
