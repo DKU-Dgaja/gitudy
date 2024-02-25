ef31497835e8b1fa33e982e23d7ff9b0b0d9985e
탁세하 <takseha98@gmail.com>
2024-02-26 00:47:47 +0900

    [AN] feat(#139): login fragment nav 구현 및 수정

--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/PopupFragment.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/PopupFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.takseha.presentation.R
+import com.takseha.presentation.databinding.FragmentAgreementPopupBinding
+
 class PopupFragment : Fragment() {
+    private lateinit var binding: FragmentAgreementPopupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/SocialLoginCompleteActivity.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/SocialLoginCompleteActivity.kt
@@ -1,12 +1,37 @@
 package com.takseha.presentation.ui.login
 
+import android.graphics.BlurMaskFilter
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.FragmentTransaction
 import com.takseha.presentation.R
-
+import com.takseha.presentation.databinding.ActivitySocialLoginCompleteBinding
 class SocialLoginCompleteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySocialLoginCompleteBinding
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_social_login_complete)
+        setBinding()
+
+        with(binding) {
+            confirmBtn.setOnClickListener {
+                val transaction = supportFragmentManager.beginTransaction()
+                val fragment = PopupFragment()
+                transaction.replace(R.id.loginPopupFragmentContainerView, fragment)
+                transaction.commit()
+            }
+        }
+    }
+
+    private fun setBinding() {
+        binding = ActivitySocialLoginCompleteBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
