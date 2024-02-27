dee781193e132d9b83ec48ef5a8884bba8a819cb
kooyeongmin <rn9dud0@naver.com>
2024-02-27 19:56:00 +0900

    [BE] feat(#140): 스터디 댓글 등록 api 구현
    * 컨트롤러, 서비스, 레포지토리 구현

---
+++ backend/src/main/java/com/example/backend/study/api/controller/comment/study/StudyCommentController.java
@@ -0,0 +1,33 @@
+package com.example.backend.study.api.controller.comment.study;
+
+import com.example.backend.auth.api.service.auth.AuthService;
+import com.example.backend.common.response.JsonResult;
+import com.example.backend.domain.define.account.user.User;
+import com.example.backend.study.api.controller.comment.study.request.StudyCommentRegisterRequest;
+import com.example.backend.study.api.service.comment.study.StudyCommentService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/study")
+public class StudyCommentController {
+    private final StudyCommentService studyCommentService;
+    private final AuthService authService;
+
+    @ApiResponse(responseCode = "200", description = "스터디 댓글 등록 성공")
+    @PostMapping("/{studyInfoId}/comment")
+    public JsonResult<?> registerStudyComment(@AuthenticationPrincipal User user,
+                                           @PathVariable("studyInfoId") Long studyInfoId,
+                                           @Valid @RequestBody StudyCommentRegisterRequest studyCommentRegisterRequest) {
+        authService.authenticate(studyCommentRegisterRequest.getUserId(), user);
+        studyCommentService.registerStudyComment(studyCommentRegisterRequest, studyInfoId);
+
+        return JsonResult.successOf("StudyComment register Success");
+    }
+}
