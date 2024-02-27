1241f476756ef5a2000248c058bedeb7d4521475
kooyeongmin <rn9dud0@naver.com>
2024-02-28 03:47:44 +0900

    [BE] feat(#144): 스터디 댓글 수정 DTO, 예외 메시지 생성

--- backend/src/main/java/com/example/backend/common/exception/ExceptionMessage.java
+++ backend/src/main/java/com/example/backend/common/exception/ExceptionMessage.java
@@ -52,7 +52,11 @@ public enum ExceptionMessage {
     TODO_NOT_FOUND("해당 Todo를 찾을 수 없습니다."),
 
     // StudyMemberException
-    USER_NOT_STUDY_MEMBER("해당 스터디원을 찾을 수 없습니다.")
+    USER_NOT_STUDY_MEMBER("해당 스터디원을 찾을 수 없습니다."),
+
+    // StudyCommentException
+    STUDY_COMMENT_NOT_FOUND("해당 스터디 댓글을 찾을 수 없습니다."),
+    STUDY_COMMENT_NOT_AUTHORIZED("스터디 댓글을 수정할 권한이 없습니다.")
     ;
     private final String text;
 }
