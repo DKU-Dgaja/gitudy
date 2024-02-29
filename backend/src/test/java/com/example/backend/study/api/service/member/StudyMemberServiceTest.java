d447d9c680f54622d6cd746e406b0ae0c1967a37
j-ra1n <j_ra1n@naver.com>
2024-02-29 19:19:25 +0900

    [BE] test(#147): 테스트
    * 활동중이지 않은 멤버가 있을때 테스

--- backend/src/test/java/com/example/backend/study/api/service/member/StudyMemberServiceTest.java
+++ backend/src/test/java/com/example/backend/study/api/service/member/StudyMemberServiceTest.java
@@ -58,10 +58,10 @@ public class StudyMemberServiceTest extends TestConfig {
         studyInfoRepository.save(studyInfo);
 
         studyMemberRepository.saveAll(List.of(
-                StudyMemberFixture.createStudyMembersByScore(leader.getId(), studyInfo.getId(), 77),
-                StudyMemberFixture.createStudyMembersByScore(activeMember1.getId(), studyInfo.getId(), 77),
-                StudyMemberFixture.createStudyMembersByScore(activeMember2.getId(), studyInfo.getId(), 99),
-                StudyMemberFixture.createStudyMembersByScore(activeMember3.getId(), studyInfo.getId(), 88)
+                StudyMemberFixture.createStudyActiveMembersByScore(leader.getId(), studyInfo.getId(), 77),
+                StudyMemberFixture.createStudyActiveMembersByScore(activeMember1.getId(), studyInfo.getId(), 77),
+                StudyMemberFixture.createStudyActiveMembersByScore(activeMember2.getId(), studyInfo.getId(), 99),
+                StudyMemberFixture.createStudyActiveMembersByScore(activeMember3.getId(), studyInfo.getId(), 88)
         ));
 
 
@@ -102,10 +102,10 @@ public class StudyMemberServiceTest extends TestConfig {
         studyInfoRepository.save(studyInfo);
 
         studyMemberRepository.saveAll(List.of(
-                StudyMemberFixture.createStudyMembersByScore(leader.getId(), studyInfo.getId(), 77),
-                StudyMemberFixture.createStudyMembersByScore(activeMember1.getId(), studyInfo.getId(), 77),
-                StudyMemberFixture.createStudyMembersByScore(activeMember2.getId(), studyInfo.getId(), 99),
-                StudyMemberFixture.createStudyMembersByScore(activeMember3.getId(), studyInfo.getId(), 88)
+                StudyMemberFixture.createStudyActiveMembersByScore(leader.getId(), studyInfo.getId(), 77),
+                StudyMemberFixture.createStudyActiveMembersByScore(activeMember1.getId(), studyInfo.getId(), 77),
+                StudyMemberFixture.createStudyActiveMembersByScore(activeMember2.getId(), studyInfo.getId(), 99),
+                StudyMemberFixture.createStudyActiveMembersByScore(activeMember3.getId(), studyInfo.getId(), 88)
         ));
 
         // when
@@ -128,4 +128,44 @@ public class StudyMemberServiceTest extends TestConfig {
         assertEquals("탁세하", responses.get(3).getName());
 
     }
+
+
+    @Test
+    @DisplayName("스터디에 속한 스터디원 조회 - 활동중이지 않은 멤버가 있는 경우 테스트")
+    public void readStudyWithdrawalMembers_userId() {
+        // given
+        boolean orderByScore = true;
+
+        User leader = UserFixture.generatePlatfomIdAndNameAndProfile("1", "이정우", "이정우프로필사진");
+        User activeMember1 = UserFixture.generatePlatfomIdAndNameAndProfile("2", "구영민", "구영민프로필사진");
+        User activeMember2 = UserFixture.generatePlatfomIdAndNameAndProfile("3", "이주성", "이주성프로필사진");
+        User activeMember3 = UserFixture.generatePlatfomIdAndNameAndProfile("4", "탁세하", "탁세하프로필사진");
+        userRepository.saveAll(List.of(leader, activeMember1, activeMember2, activeMember3));
+
+        StudyInfo studyInfo = StudyInfoFixture.createDefaultPublicStudyInfo(leader.getId());
+        studyInfoRepository.save(studyInfo);
+
+        // 영민,세하 활동중이지 않은 멤버 지정
+        studyMemberRepository.saveAll(List.of(
+                StudyMemberFixture.createStudyActiveMembersByScore(leader.getId(), studyInfo.getId(), 77),
+                StudyMemberFixture.createStudyWithdrawalMembersByScore(activeMember1.getId(), studyInfo.getId(), 77),
+                StudyMemberFixture.createStudyActiveMembersByScore(activeMember2.getId(), studyInfo.getId(), 99),
+                StudyMemberFixture.createStudyWithdrawalMembersByScore(activeMember3.getId(), studyInfo.getId(), 88)
+        ));
+
+        // when
+        List<StudyMembersResponse> responses = studyMemberService.readStudyMembers(studyInfo.getId(), orderByScore);
+
+        // then
+        assertNotNull(responses);
+        assertEquals(2, responses.size());
+
+        assertEquals(99, responses.get(0).getScore());
+        assertEquals("이주성", responses.get(0).getName());
+
+        assertEquals(77, responses.get(1).getScore());
+        assertEquals("이정우", responses.get(1).getName());
+
+    }
+
 }
