a37ea4bce4ba7112f4e4db30a2131609ec7f6aec
kooyeongmin <rn9dud0@naver.com>
2024-03-01 13:26:49 +0900

    [BE] test(#158): 쿼리 테스트

---
+++ backend/src/test/java/com/example/backend/domain/define/study/comment/study/repository/StudyCommentRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.example.backend.domain.define.study.comment.study.repository;
+
+import com.example.backend.auth.TestConfig;
+import com.example.backend.auth.config.fixture.UserFixture;
+import com.example.backend.domain.define.account.user.User;
+import com.example.backend.domain.define.study.comment.StudyCommentFixture;
+import com.example.backend.domain.define.study.comment.study.StudyComment;
+import com.example.backend.domain.define.study.info.StudyInfo;
+import com.example.backend.domain.define.study.info.StudyInfoFixture;
+import com.example.backend.domain.define.study.info.repository.StudyInfoRepository;
+import com.example.backend.study.api.controller.comment.study.response.StudyCommentResponse;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.backend.domain.define.account.user.repository.UserRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SuppressWarnings("NonAsciiCharacters")
+class StudyCommentRepositoryTest extends TestConfig {
+    private final static int DATA_SIZE = 20;
+    private final static Long LIMIT = 10L;
+
+    @Autowired
+    private StudyCommentRepository studyCommentRepository;
+
+    @Autowired
+    private StudyInfoRepository studyInfoRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @AfterEach
+    void tearDown() {
+        studyCommentRepository.deleteAllInBatch();
+        studyInfoRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+    }
+
+    @Test
+    void 스터디_댓글_리스트_조회_쿼리_테스트() {
+        // given
+        Long cursorIdx = 25L;
+
+        User user = userRepository.save(UserFixture.generateAuthUser());
+        StudyInfo study = studyInfoRepository.save(StudyInfoFixture.createDefaultPublicStudyInfo(user.getId()));
+
+        List<StudyComment> StudyCommentList = StudyCommentFixture.createDefaultStudyCommentList(DATA_SIZE, user.getId(), study.getId());
+        studyCommentRepository.saveAll(StudyCommentList);
+
+        // when
+        List<StudyCommentResponse> StudyCommentListResponse = studyCommentRepository.findStudyCommentListByUserIdJoinStudyInfo(user.getId(), cursorIdx, LIMIT);
+
+        // then
+        for (StudyCommentResponse b : StudyCommentListResponse) {
+            assertTrue(b.getId() < cursorIdx);
+            assertEquals(user.getId(), b.getUserInfoResponse().getUserId());
+            assertEquals(user.getGithubId(), b.getUserInfoResponse().getGithubId());
+        }
+    }
+
+
+    @Test
+    void cursor가_null인_경우_스터디_댓글_리스트_조회_쿼리_테스트() {
+        // given
+        User user = userRepository.save(UserFixture.generateAuthUser());
+        StudyInfo study = studyInfoRepository.save(StudyInfoFixture.createDefaultPublicStudyInfo(user.getId()));
+
+        List<StudyComment> StudyCommentList = StudyCommentFixture.createDefaultStudyCommentList(DATA_SIZE, user.getId(), study.getId());
+        studyCommentRepository.saveAll(StudyCommentList);
+
+        // when
+        List<StudyCommentResponse> StudyCommentListResponse = studyCommentRepository.findStudyCommentListByUserIdJoinStudyInfo(user.getId(), null, LIMIT);
+
+        // then
+        assertEquals(LIMIT, StudyCommentListResponse.size());
+        for (StudyCommentResponse b : StudyCommentListResponse) {
+            assertEquals(user.getId(), b.getUserInfoResponse().getUserId());
+            assertEquals(user.getGithubId(), b.getUserInfoResponse().getGithubId());
+        }
+    }
+}
