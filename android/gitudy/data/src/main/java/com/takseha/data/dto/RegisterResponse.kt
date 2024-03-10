0d3da8be8e240db3dcebb129460acdebfdc4ced8
탁세하 <takseha98@gmail.com>
2024-03-10 16:31:16 +0900

    [AN] refactor(#139): role 관련 기능 수정

--- android/gitudy/data/src/main/java/com/takseha/data/dto/RegisterResponse.kt
+++ android/gitudy/data/src/main/java/com/takseha/data/dto/RegisterResponse.kt
@@ -7,7 +7,5 @@ data class RegisterResponse(
     @SerializedName("accessToken")
     val accessToken: String,
     @SerializedName("refreshToken")
-    val refreshToken: String,
-    @SerializedName("role")
-    val role: String
+    val refreshToken: String
 )
--- android/gitudy/presentation/src/main/java/com/takseha/presentation/viewmodel/LoginWebViewViewModel.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/viewmodel/LoginWebViewViewModel.kt
@@ -3,9 +3,12 @@ package com.takseha.presentation.viewmodel
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.takseha.common.model.SPKey
 import com.takseha.common.util.SP
+import com.takseha.data.dto.Role
 import com.takseha.data.repository.GitudyRepository
 import kotlinx.coroutines.launch
 
@@ -13,6 +16,10 @@ import kotlinx.coroutines.launch
 class LoginWebViewViewModel(application: Application) : AndroidViewModel(application) {
     private val gitudyRepository = GitudyRepository()
     private lateinit var prefs: SP
+
+    private var _role = MutableLiveData<String>()
+    val role : LiveData<String>
+        get() = _role
 
     fun saveAllTokens(platformType: String, code: String, state: String) = viewModelScope.launch {
         prefs = SP(getApplication())
@@ -33,6 +40,8 @@ class LoginWebViewViewModel(application: Application) : AndroidViewModel(applica
                 SPKey.REFRESH_TOKEN,
                 allTokens.refreshToken
             )
+            _role.value = allTokens.role
+
             Log.d("GetTokenViewModel", "shared pref 저장된 access token: ${prefs.loadPref(SPKey.ACCESS_TOKEN, "0")}\n"
                     + "shared pref 저장된 refresh token: ${prefs.loadPref(SPKey.REFRESH_TOKEN, "0")}")
         } else {
