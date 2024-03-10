b721f6d99674a3de867e935fe94d828ded8a8c41
탁세하 <takseha98@gmail.com>
2024-03-11 05:03:40 +0900

    [AN] refactor(#139): 추후 리팩토링 시 구현해야 할 부분 정리

--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/SplashActivity.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/SplashActivity.kt
@@ -10,6 +10,12 @@ import com.takseha.presentation.R
 import com.takseha.presentation.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
+    /*
+     리팩토링 시 구현해야 할 것
+     1. splash 화면에서 info api 연동해서 role 확인 -> 자동로그인 기능 구현(로그인 및 회원가입 화면 pass 하는 로직 구현)
+     2. 닉네임 특수기호(이모티콘) 입력 불가하도록 하는 기능 추가
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
