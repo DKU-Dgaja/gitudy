ed600cf7320d3f1948b37e2819c157671a455176
탁세하 <takseha98@gmail.com>
2024-03-11 04:47:27 +0900

    [AN] feat(#139): RegisterCompleteFragment 캐릭터 애니메이션 구현

--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/RegisterCompleteFragment.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/login/RegisterCompleteFragment.kt
@@ -6,10 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.core.content.ContextCompat
+import com.takseha.common.model.SPKey
+import com.takseha.common.util.SP
 import com.takseha.presentation.R
+import com.takseha.presentation.databinding.FragmentRegisterCompleteBinding
 
 class RegisterCompleteFragment : Fragment() {
+    private var _binding : FragmentRegisterCompleteBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var prefs: SP
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requireActivity().window.statusBarColor =  Color.argb(0xFF,0x15,0x14,0x1B)
@@ -19,7 +27,25 @@ class RegisterCompleteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_complete, container, false)
+        _binding = FragmentRegisterCompleteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        prefs = SP(requireActivity().applicationContext)
+
+        with(binding) {
+            nickname.text = prefs.loadPref(SPKey.GITUDY_NAME, "0")
+
+            val characterAnim = AnimationUtils.loadAnimation(context, R.anim.fade_in)
+            characterLayout.startAnimation(characterAnim)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
