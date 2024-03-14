7e710677c5635b640cad4056b8fdf7304f4dd0cb
kooyeongmin <rn9dud0@naver.com>
2024-03-14 11:57:54 +0900

    [BE] refactor(#196): 주석 변경

--- backend/src/main/java/com/example/backend/domain/define/study/category/info/repository/StudyCategoryRepositoryCustom.java
+++ backend/src/main/java/com/example/backend/domain/define/study/category/info/repository/StudyCategoryRepositoryCustom.java
@@ -3,6 +3,6 @@ package com.example.backend.domain.define.study.category.info.repository;
 import java.util.List;
 
 public interface StudyCategoryRepositoryCustom {
-    // studyInfoId를 통해 스터디들의 모든 이름을 조회한다.
+    // studyInfoId를 통해 카테고리들의 모든 이름을 조회한다.
     List<String> findCategoryNameListByStudyInfoJoinCategoryMapping(Long studyInfoId);
 }
