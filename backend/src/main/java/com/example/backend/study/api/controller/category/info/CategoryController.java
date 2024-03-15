eed4091b85995e60c7c95f19beb4c16dea5abdc1
kooyeongmin <rn9dud0@naver.com>
2024-03-15 16:46:34 +0900

    [BE] feat(#207): 카테고리 수정삭제 api 구현

--- backend/src/main/java/com/example/backend/study/api/controller/category/info/CategoryController.java
+++ backend/src/main/java/com/example/backend/study/api/controller/category/info/CategoryController.java
@@ -4,16 +4,14 @@ import com.example.backend.auth.api.service.auth.AuthService;
 import com.example.backend.common.response.JsonResult;
 import com.example.backend.domain.define.account.user.User;
 import com.example.backend.study.api.controller.category.info.request.CategoryRegisterRequest;
+import com.example.backend.study.api.controller.category.info.request.CategoryUpdateRequest;
 import com.example.backend.study.api.service.category.info.CategoryService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -30,5 +28,23 @@ public class CategoryController {
         authService.authenticate(categoryRegisterRequest.getUserId(), user);
         categoryService.registerCategory(categoryRegisterRequest);
         return JsonResult.successOf("Category Register Success.");
+    }
+    @ApiResponse(responseCode = "200", description = "카테고리 수정 성공")
+    @PatchMapping("/{categoryId}")
+    public JsonResult<?> updateStudyComment(@AuthenticationPrincipal User user,
+                                            @PathVariable(name = "categoryId") Long categoryId,
+                                            @Valid @RequestBody CategoryUpdateRequest categoryUpdateRequest) {
+        authService.authenticate(categoryUpdateRequest.getUserId(), user);
+        categoryService.updateCategory(categoryUpdateRequest, categoryId);
+        return JsonResult.successOf("Category update Success");
+    }
+    @ApiResponse(responseCode = "200", description = "카테고리 삭제 성공")
+    @DeleteMapping("/{categoryId}")
+    public JsonResult<?> deleteStudyComment(@AuthenticationPrincipal User user,
+                                            @PathVariable(name = "categoryId") Long categoryId
+    ) {
+        authService.findUserInfo(user);
+        categoryService.deleteCategory(categoryId);
+        return JsonResult.successOf("Category deleted successfully");
     }
 }
--- backend/src/main/java/com/example/backend/study/api/service/category/info/CategoryService.java
+++ backend/src/main/java/com/example/backend/study/api/service/category/info/CategoryService.java
@@ -1,8 +1,17 @@
 package com.example.backend.study.api.service.category.info;
 
+import com.example.backend.common.exception.ExceptionMessage;
+import com.example.backend.common.exception.category.CategoryException;
+import com.example.backend.common.exception.comment.study.StudyCommentException;
+import com.example.backend.common.exception.study.StudyInfoException;
+import com.example.backend.common.exception.user.UserException;
+import com.example.backend.domain.define.account.user.User;
 import com.example.backend.domain.define.study.category.info.StudyCategory;
 import com.example.backend.domain.define.study.category.info.repository.StudyCategoryRepository;
+import com.example.backend.domain.define.study.comment.study.StudyComment;
 import com.example.backend.study.api.controller.category.info.request.CategoryRegisterRequest;
+import com.example.backend.study.api.controller.category.info.request.CategoryUpdateRequest;
+import com.example.backend.study.api.controller.comment.study.request.StudyCommentUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +33,23 @@ public class CategoryService {
                 .build();
 
         studyCategoryRepository.save(studyCategory);
+    }
+
+    @Transactional
+    public void updateCategory(CategoryUpdateRequest request, Long categoryId) {
+        StudyCategory studyCategory = studyCategoryRepository.findById(categoryId).orElseThrow(() -> {
+            log.warn(">>>> {} : {} <<<<", categoryId, ExceptionMessage.CATEGORY_NOT_FOUND.getText());
+            throw new CategoryException(ExceptionMessage.CATEGORY_NOT_FOUND);
+        });
+        studyCategory.updateCategory(request.getName());
+    }
+
+    @Transactional
+    public void deleteCategory(Long categoryId) {
+        StudyCategory studyCategory = studyCategoryRepository.findById(categoryId).orElseThrow(() -> {
+            log.warn(">>>> {} : {} <<<<", categoryId, ExceptionMessage.CATEGORY_NOT_FOUND.getText());
+            throw new CategoryException(ExceptionMessage.CATEGORY_NOT_FOUND);
+        });
+        studyCategoryRepository.deleteById(studyCategory.getId());
     }
 }
