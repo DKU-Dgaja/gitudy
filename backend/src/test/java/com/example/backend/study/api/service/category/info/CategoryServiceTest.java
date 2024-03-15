1392c1bb3e4c2fa21b0b040c0f30c380f108b632
kooyeongmin <rn9dud0@naver.com>
2024-03-15 16:47:31 +0900

    [BE] test(#207): 카테고리 수정, 삭제 테스트

--- backend/src/test/java/com/example/backend/study/api/service/category/info/CategoryServiceTest.java
+++ backend/src/test/java/com/example/backend/study/api/service/category/info/CategoryServiceTest.java
@@ -1,16 +1,21 @@
 package com.example.backend.study.api.service.category.info;
 
 import com.example.backend.auth.TestConfig;
+import com.example.backend.common.exception.ExceptionMessage;
+import com.example.backend.common.exception.category.CategoryException;
 import com.example.backend.domain.define.account.user.User;
 import com.example.backend.domain.define.account.user.repository.UserRepository;
+import com.example.backend.domain.define.study.StudyCategory.info.StudyCategoryFixture;
 import com.example.backend.domain.define.study.category.info.StudyCategory;
 import com.example.backend.domain.define.study.category.info.repository.StudyCategoryRepository;
 import com.example.backend.study.api.controller.category.info.request.CategoryRegisterRequest;
+import com.example.backend.study.api.controller.category.info.request.CategoryUpdateRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.example.backend.auth.config.fixture.UserFixture.generateAuthUser;
 import static org.junit.jupiter.api.Assertions.*;
@@ -47,5 +52,74 @@ class CategoryServiceTest extends TestConfig {
         // then
         List<StudyCategory> savedStudyCategories = studyCategoryRepository.findAll();
         assertEquals(savedStudyCategories.size(), 1);
+    }
+
+    @Test
+    void 카테고리_수정_테스트() {
+        // given
+        String updatedName = "update";
+        User user = userRepository.save(generateAuthUser());
+
+        StudyCategory studyCategory
+                = studyCategoryRepository.save(StudyCategoryFixture.createDefaultPublicStudyCategory("name"));
+        CategoryUpdateRequest request = CategoryUpdateRequest.builder()
+                .userId(user.getId())
+                .name(updatedName)
+                .build();
+
+        //when
+        categoryService.updateCategory(request, studyCategory.getId());
+
+        // then
+        Optional<StudyCategory> savedStudyCategory = studyCategoryRepository.findById(studyCategory.getId());
+        assertEquals(savedStudyCategory.get().getName(), updatedName);
+    }
+    @Test
+    void 카테고리_수정_예외_테스트_카테고리_없음() {
+        // given
+        Long invaildCategoryId = 987654L;
+        String updatedName = "update";
+        User user = userRepository.save(generateAuthUser());
+
+        StudyCategory studyCategory
+                = studyCategoryRepository.save(StudyCategoryFixture.createDefaultPublicStudyCategory("name"));
+        CategoryUpdateRequest request = CategoryUpdateRequest.builder()
+                .userId(user.getId())
+                .name(updatedName)
+                .build();
+
+        // when, then
+        assertThrows(CategoryException.class, () -> {
+            categoryService.updateCategory(request, invaildCategoryId);
+        }, ExceptionMessage.CATEGORY_NOT_FOUND.getText());
+    }
+    @Test
+    void 카테고리_삭제_테스트() {
+        // given
+        userRepository.save(generateAuthUser());
+
+        StudyCategory studyCategory
+                = studyCategoryRepository.save(StudyCategoryFixture.createDefaultPublicStudyCategory("name"));
+
+        //when
+        categoryService.deleteCategory(studyCategory.getId());
+
+        // then
+        Optional<StudyCategory> savedStudyCategory = studyCategoryRepository.findById(studyCategory.getId());
+        assertTrue(savedStudyCategory.isEmpty());
+    }
+
+    @Test
+    void 카테고리_삭제_예외_테스트_카테고리_없음() {
+        // given
+        Long invaildCategoryId = 987654L;
+        userRepository.save(generateAuthUser());
+
+        studyCategoryRepository.save(StudyCategoryFixture.createDefaultPublicStudyCategory("name"));
+
+        // when, then
+        assertThrows(CategoryException.class, () -> {
+            categoryService.deleteCategory(invaildCategoryId);
+        }, ExceptionMessage.CATEGORY_NOT_FOUND.getText());
     }
 }
