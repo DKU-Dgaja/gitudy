8b6a2b91de6ec44a60ce4c8369c9da08a581a1e7
이주성 <32183520@dankook.ac.kr>
2024-03-18 00:27:23 +0900

    [BE] feat(#205): 불러온 커밋들 중 컨벤션을 통과한 커밋들을 저장하는 로직 구현

--- backend/src/main/java/com/example/backend/study/api/service/commit/StudyCommitService.java
+++ backend/src/main/java/com/example/backend/study/api/service/commit/StudyCommitService.java
@@ -2,24 +2,41 @@ package com.example.backend.study.api.service.commit;
 
 import com.example.backend.common.exception.ExceptionMessage;
 import com.example.backend.common.exception.commit.CommitException;
+import com.example.backend.common.exception.convention.ConventionException;
+import com.example.backend.common.exception.user.UserException;
+import com.example.backend.domain.define.account.user.User;
+import com.example.backend.domain.define.account.user.repository.UserRepository;
 import com.example.backend.domain.define.study.commit.StudyCommit;
 import com.example.backend.domain.define.study.commit.repository.StudyCommitRepository;
+import com.example.backend.domain.define.study.convention.StudyConvention;
+import com.example.backend.domain.define.study.convention.repository.StudyConventionRepository;
+import com.example.backend.domain.define.study.info.StudyInfo;
+import com.example.backend.domain.define.study.todo.info.StudyTodo;
+import com.example.backend.study.api.controller.convention.response.StudyConventionResponse;
 import com.example.backend.study.api.service.commit.response.CommitInfoResponse;
+import com.example.backend.study.api.service.convention.StudyConventionService;
+import com.example.backend.study.api.service.github.GithubApiService;
+import com.example.backend.study.api.service.github.response.GithubCommitResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class StudyCommitService {
+    private final UserRepository userRepository;
     private final static Long MAX_LIMIT = 50L;
 
     private final StudyCommitRepository studyCommitRepository;
+    private final GithubApiService githubApiService;
+    private final StudyConventionService studyConventionService;
+    private final StudyConventionRepository studyConventionRepository;
 
     public CommitInfoResponse getCommitDetailsById(Long commitId) {
         StudyCommit commit = studyCommitRepository.findById(commitId).orElseThrow(() -> {
@@ -35,5 +52,34 @@ public class StudyCommitService {
         limit = Math.min(limit, MAX_LIMIT);
 
         return studyCommitRepository.findStudyCommitListByUserId_CursorPaging(userId, studyId, cursorIdx, limit);
+    }
+
+    // 커밋 업데이트
+    @Transactional
+    public void fetchRemoteCommitsAndSave(StudyInfo study, StudyTodo todo) {
+        // 원격의 커밋들 중 저장되지 않은 커밋 불러오기
+        List<GithubCommitResponse> unsavedCommits = githubApiService.pullUnsavedCommits(study.getRepositoryInfo(), todo);
+
+        // 컨벤션 검증 후 통과한 커밋만 StudyCommit으로 저장
+        StudyConventionResponse conventions = studyConventionRepository.findActiveConventionByStudyInId(study.getId());
+        List<StudyCommit> commitList = unsavedCommits.stream()
+                .filter(commit -> studyConventionService.checkConvention(conventions.getContent(), commit.getMessage()))
+                .flatMap(commit -> {    // user를 못 찾은 경우 무시하고 다음 커밋으로 넘어가도록 설정
+                    try {
+                        User findUser = userRepository.findByGithubId(commit.getAuthorName()).orElseThrow(() -> {
+                            log.warn(">>>> User not found with GithubId: {}", commit.getAuthorName());
+                            return new UserException(ExceptionMessage.USER_NOT_FOUND);
+                        });
+                        return Stream.of(StudyCommit.of(findUser.getId(), todo, commit));
+                    } catch (UserException e) {
+                        // user를 찾을 수 없는 경우 처리
+                        log.warn("User not found for commit: {}", commit.getMessage());
+                        return Stream.empty();
+                    }
+                })
+                .toList();
+
+        studyCommitRepository.saveAll(commitList);
+
     }
 }
--- backend/src/test/java/com/example/backend/study/api/service/commit/StudyCommitServiceTest.java
+++ backend/src/test/java/com/example/backend/study/api/service/commit/StudyCommitServiceTest.java
@@ -1,20 +1,40 @@
 package com.example.backend.study.api.service.commit;
 
 import com.example.backend.auth.TestConfig;
+import com.example.backend.auth.config.fixture.UserFixture;
+import com.example.backend.common.exception.ExceptionMessage;
 import com.example.backend.common.exception.commit.CommitException;
+import com.example.backend.common.exception.user.UserException;
+import com.example.backend.domain.define.account.user.User;
+import com.example.backend.domain.define.account.user.repository.UserRepository;
 import com.example.backend.domain.define.study.commit.StudyCommit;
 import com.example.backend.domain.define.study.commit.StudyCommitFixture;
 import com.example.backend.domain.define.study.commit.repository.StudyCommitRepository;
+import com.example.backend.domain.define.study.convention.StudyConvention;
+import com.example.backend.domain.define.study.convention.StudyConventionFixture;
+import com.example.backend.domain.define.study.convention.repository.StudyConventionRepository;
+import com.example.backend.domain.define.study.info.StudyInfo;
+import com.example.backend.domain.define.study.info.StudyInfoFixture;
+import com.example.backend.domain.define.study.info.constant.RepositoryInfo;
+import com.example.backend.domain.define.study.info.constant.StudyPeriodType;
+import com.example.backend.domain.define.study.info.constant.StudyStatus;
+import com.example.backend.domain.define.study.info.repository.StudyInfoRepository;
+import com.example.backend.domain.define.study.todo.StudyTodoFixture;
+import com.example.backend.domain.define.study.todo.info.StudyTodo;
+import com.example.backend.domain.define.study.todo.repository.StudyTodoRepository;
 import com.example.backend.study.api.service.commit.response.CommitInfoResponse;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
+import static com.example.backend.domain.define.account.user.constant.UserPlatformType.GITHUB;
+import static com.example.backend.domain.define.account.user.constant.UserRole.USER;
 import static com.example.backend.domain.define.study.commit.StudyCommitFixture.createDefaultStudyCommitList;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,16 +42,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudyCommitServiceTest extends TestConfig {
     private final static int DATA_SIZE = 10;
     private final static Long LIMIT = 10L;
+    private final String REPOSITORY_OWNER = "jusung-c";
+    private final String REPOSITORY_NAME = "Github-Api-Test";
 
     @Autowired
     private StudyCommitService studyCommitService;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private StudyCommitRepository studyCommitRepository;
+
+    @Autowired
+    private StudyInfoRepository studyInfoRepository;
+
+    @Autowired
+    private StudyTodoRepository studyTodoRepository;
+
+    @Autowired
+    private StudyConventionRepository studyConventionRepository;
 
     @AfterEach
     void tearDown() {
+        userRepository.deleteAllInBatch();
         studyCommitRepository.deleteAllInBatch();
+        studyInfoRepository.deleteAllInBatch();
+        studyTodoRepository.deleteAllInBatch();
+        studyInfoRepository.deleteAllInBatch();
     }
 
     @Test
@@ -108,4 +146,70 @@ class StudyCommitServiceTest extends TestConfig {
             studyCommitService.getCommitDetailsById(commitID);
         });
     }
+
+    @Test
+    void 레포지토리에서_커밋_fetch_성공_테스트() {
+        // given
+        // 유저 저장
+        User user = userRepository.save(User.builder()
+                .platformId("1")
+                .platformType(GITHUB)
+                .role(USER)
+                .name("이름")
+                .githubId(REPOSITORY_OWNER)
+                .profileImageUrl("프로필이미지")
+                .build());
+
+        // 스터디 저장
+        StudyInfo study = studyInfoRepository.save(StudyInfo.builder()
+                .userId(user.getId())
+                .topic("topic")
+                .status(StudyStatus.STUDY_PUBLIC)
+                .repositoryInfo(RepositoryInfo.builder()
+                        .owner(REPOSITORY_OWNER)
+                        .name(REPOSITORY_NAME)
+                        .branchName("main")
+                        .build())
+                .build());
+
+        // 투두 저장
+        StudyTodo todo = studyTodoRepository.save(StudyTodoFixture.createStudyTodo(study.getId()));
+
+        // 컨벤션 저장
+        String conventionName = "커밋 메세지 규칙";
+        String convention = "^\\[[A-Za-z가-힣0-9]+\\] [A-Za-z가-힣]+: .+$";
+        String conventionDescription = "커밋 메세지 규칙: [이름] 플랫폼 \":\" + \" \" + 문제 이름 \n" +
+                "예시 1) [이주성] 백준: 크리스마스 트리 \n" +
+                "예시 2) [이주성] 프로그래머스: 두 수의 곱";
+
+        studyConventionRepository.save(StudyConvention.builder()
+                .studyInfoId(1L)
+                .name(conventionName)
+                .description(conventionDescription)
+                .content(convention)
+                .isActive(true)
+                .build());
+
+        // 현재 "백준 1234 풀기" 폴더에 저장되어 있는 테스트 커밋 5개, 이 중 컨벤션을 지킨 커밋은 1개
+        // [이주성] 백준 1234번 크리스마스 트리
+        // [이주성] 백준: 1234번 크리스마스 트리 -> 컨벤션 지킨 유일한 커밋
+        // Create test
+        // 백준 1234번 풀기 directory
+        // TODO(백준 1234번 풀기)가 생성되었습니다.
+        int expectedSize = 1;
+        String expectedMsg = "[이주성] 백준: 1234번 크리스마스 트리";
+
+        // when
+        studyCommitService.fetchRemoteCommitsAndSave(study, todo);
+        List<StudyCommit> allCommits = studyCommitRepository.findAll();
+
+        // then
+        assertEquals(allCommits.size(), expectedSize);
+        for (var c : allCommits) {
+            assertEquals(c.getStudyInfoId(), study.getUserId());
+            assertEquals(c.getUserId(), user.getId());
+        }
+        assertEquals(allCommits.get(0).getMessage(), expectedMsg);
+    }
+
 }
