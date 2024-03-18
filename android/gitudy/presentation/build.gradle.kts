ae92fb30ca13e1f6bf395b00bd14dfee1f9e50b8
탁세하 <takseha98@gmail.com>
2024-03-18 16:12:01 +0900

    [AN] feat(#200): 메인 홈 관련 UI 수정 및 애니메이션 구현

--- android/gitudy/presentation/build.gradle.kts
+++ android/gitudy/presentation/build.gradle.kts
@@ -79,7 +79,4 @@ dependencies {
     // retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    // paging
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
 }
