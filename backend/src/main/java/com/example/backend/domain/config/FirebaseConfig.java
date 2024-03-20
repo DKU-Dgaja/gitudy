81c43c9c3194336a3ddb5b72cfab623bc3408bfa
j-ra1n <j_ra1n@naver.com>
2024-03-20 16:55:49 +0900

    [BE] setting(#229): FirebaseConfig 추가

---
+++ backend/src/main/java/com/example/backend/domain/config/FirebaseConfig.java
@@ -0,0 +1,39 @@
+package com.example.backend.domain.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${firebase.key-path}")
+    String fcmKeyPath;
+
+    @PostConstruct
+    public void getFcmCredential() {
+        try {
+            if (FirebaseApp.getApps().isEmpty()) {
+                InputStream refreshToken = new ClassPathResource(fcmKeyPath).getInputStream();
+
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
+
+                FirebaseApp.initializeApp(options);
+                log.info("Fcm Setting Completed");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+}
