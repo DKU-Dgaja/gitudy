e32f98dc467fd67fac97ba931a6a833c64c5bcba
j-ra1n <j_ra1n@naver.com>
2024-03-20 16:17:31 +0900

    [BE] setting(#227): config 수정

--- backend/src/main/java/com/example/backend/auth/config/firebase/FirebaseConfig.java
+++ backend/src/main/java/com/example/backend/auth/config/firebase/FirebaseConfig.java
@@ -4,24 +4,34 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+@Slf4j
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.key-path}")
+    String fcmKeyPath;
+
     @PostConstruct
-    public void init(){
-        try{
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+    public void getFcmCredential() {
+        try {
+            InputStream refreshToken = new ClassPathResource(fcmKeyPath).getInputStream();
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
+
             FirebaseApp.initializeApp(options);
-        }catch (Exception e){
-            e.printStackTrace();
+            log.info("Fcm Setting Completed");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
         }
     }
 }
