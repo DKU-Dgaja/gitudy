8394dad02fb275321527b9fa4e6ecf1f69146f0c
j-ra1n <j_ra1n@naver.com>
2024-03-20 15:24:53 +0900

    [BE] setting(#227): firebase 초기세팅
    * config 추가

---
+++ backend/src/main/java/com/example/backend/auth/config/firebase/FirebaseConfig.java
@@ -0,0 +1,27 @@
+package com.example.backend.auth.config.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init(){
+        try{
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
