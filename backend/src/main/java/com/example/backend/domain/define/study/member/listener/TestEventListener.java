1cab4a0eed739de9b68d6cc1135e8cb1dc04c015
kooyeongmin <rn9dud0@naver.com>
2024-03-22 13:07:07 +0900

    [BE] refactor(#225): 비동기 추가

--- backend/src/main/java/com/example/backend/domain/define/study/member/listener/TestEventListener.java
+++ backend/src/main/java/com/example/backend/domain/define/study/member/listener/TestEventListener.java
@@ -4,16 +4,19 @@ import com.example.backend.study.api.event.TestEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class TestEventListener {
+
+    @Async
     @EventListener
     public void testListener(TestEvent testEvent) {
         log.info("Received TestEvent: {}", testEvent);
-        // TODO : 이벤트 로직 구현
+        // TODO : FCM서버에 알람 요청 로직 구현
 
 
 
