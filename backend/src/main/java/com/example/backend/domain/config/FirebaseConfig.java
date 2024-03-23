fe6462e425ceecf39f3c9235c466861c0523c019
j-ra1n <j_ra1n@naver.com>
2024-03-23 21:50:29 +0900

    [BE] setting(#229): config 수정

--- backend/src/main/java/com/example/backend/domain/config/FirebaseConfig.java
+++ backend/src/main/java/com/example/backend/domain/config/FirebaseConfig.java
@@ -9,31 +9,33 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
 
     @Value("${firebase.key-path}")
-    String fcmKeyPath;
+    private String fcmKeyPath;
 
-    @Value("${firebase.project-id}")
-    String projectId;
 
     @PostConstruct
     public void getFcmCredential() {
         try {
             if (FirebaseApp.getApps().isEmpty()) {
-                InputStream credentials = new ClassPathResource(fcmKeyPath).getInputStream();
+                GoogleCredentials credentials = GoogleCredentials
+                        .fromStream(new ClassPathResource(fcmKeyPath).getInputStream());
 
-                FirebaseOptions options = FirebaseOptions.builder()
-                        .setCredentials(GoogleCredentials.fromStream(credentials))
-                        .setProjectId(projectId)
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(credentials)
                         .build();
 
                 FirebaseApp.initializeApp(options);
+
+
                 log.info("Fcm Setting Completed");
             }
         } catch (IOException e) {
