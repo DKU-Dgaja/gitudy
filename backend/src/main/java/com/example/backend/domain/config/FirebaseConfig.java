0ffcf044f27f2a5ef5d81eb9be0d93915a11abff
j-ra1n <j_ra1n@naver.com>
2024-03-26 23:15:33 +0900

    [BE] refactor(#244): firebaseconfig 수정

--- backend/src/main/java/com/example/backend/domain/config/FirebaseConfig.java
+++ backend/src/main/java/com/example/backend/domain/config/FirebaseConfig.java
@@ -3,14 +3,17 @@ package com.example.backend.domain.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -20,23 +23,30 @@ public class FirebaseConfig {
     private String fcmKeyPath;
 
 
-    @PostConstruct
-    public void getFcmCredential() {
-        try {
-            if (FirebaseApp.getApps().isEmpty()) {
-                InputStream credentials = new ClassPathResource(fcmKeyPath).getInputStream();
+    @Bean
+    public FirebaseMessaging firebaseMessaging() throws IOException {
+        ClassPathResource resource = new ClassPathResource(fcmKeyPath);
+        InputStream refreshToken = resource.getInputStream();
 
-                FirebaseOptions options = new FirebaseOptions.Builder()
-                        .setCredentials(GoogleCredentials.fromStream(credentials))
-                        .build();
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
 
-                FirebaseApp.initializeApp(options);
-                log.info("Fcm Setting Completed");
+        if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
+            for(FirebaseApp app: firebaseAppList) {
+                if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                    firebaseApp = app;
+                }
             }
-        } catch (IOException e) {
-            log.info(">>>>>>>>FCM error");
-            log.error(">>>>>>FCM error message : " + e.getMessage());
         }
+
+        if(firebaseApp == null) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                    .build();
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+
+        return FirebaseMessaging.getInstance(firebaseApp);
     }
 
 }
