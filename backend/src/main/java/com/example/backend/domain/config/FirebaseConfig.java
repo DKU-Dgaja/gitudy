d60614db9cafb5887cfeab8cefa2b1a39d9769eb
j-ra1n <j_ra1n@naver.com>
2024-03-28 18:22:31 +0900

    [BE] refactor(#244): 오류수정

--- backend/src/main/java/com/example/backend/domain/config/FirebaseConfig.java
+++ backend/src/main/java/com/example/backend/domain/config/FirebaseConfig.java
@@ -46,20 +46,16 @@ public class FirebaseConfig {
     }*/
 
     @Bean
-    public FirebaseMessaging firebaseMessaging() {
+    public FirebaseMessaging firebaseMessaging() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            try {
-                // FirebaseApp 초기화 로직
-                InputStream credentials = new ClassPathResource(fcmKeyPath).getInputStream();
-                String text = new String(credentials.readAllBytes(), StandardCharsets.UTF_8);
-                FirebaseOptions options = new FirebaseOptions.Builder()
-                        .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8))))
-                        .build();
-                FirebaseApp.initializeApp(options);
-            } catch (IOException e) {
-                log.error("Failed to initialize FirebaseApp", e);
-                throw new IllegalStateException("Failed to initialize FirebaseApp", e);
-            }
+            // FirebaseApp 초기화 로직
+            InputStream credentials = new ClassPathResource(fcmKeyPath).getInputStream();
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(credentials))
+                    .build();
+            FirebaseApp.initializeApp(options);
+
         }
 
         log.info("FCM Setting Completed");
