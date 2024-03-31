c0b3f1a2e9cc2397aec3faa20ea23122092e2318
탁세하 <takseha98@gmail.com>
2024-03-31 23:33:38 +0900

    [AN] feat(#232): 스터디 생성 관련 viewModel 수정 및 fragment1, 2 기능 구현

--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/feed/MakeStudy3Fragment.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/feed/MakeStudy3Fragment.kt
@@ -45,7 +45,7 @@ class MakeStudy3Fragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MakeStudy3Fragment.
+         * @return A new instance of fragment MakeStudy4Fragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
