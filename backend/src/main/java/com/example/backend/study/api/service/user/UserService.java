f99cf5e3518b2e03e18ddae6d917973833a399cc
j-ra1n <j_ra1n@naver.com>
2024-04-29 21:00:10 +0900

    [BE] refactor(#275): 피드백반영
    * 멤버모두 승인거부시 fcm 전달 예외처리 발생 제거

--- backend/src/main/java/com/example/backend/study/api/service/user/UserService.java
+++ backend/src/main/java/com/example/backend/study/api/service/user/UserService.java
@@ -51,12 +51,6 @@ public class UserService {
     // isPushAlarmYn가 true인 사용자의 ID 목록을 반환하는 메서드
     public List<Long> findIsPushAlarmYsByIdsOrThrowException(List<Long> userIds) {
 
-        // users 비어있는경우 예외처리
-        if (userIds == null || userIds.isEmpty()) {
-            log.warn(">>>> {} : {} <<<<", userIds, ExceptionMessage.USER_NOT_FOUND_ALARM_Y);
-            throw new EventException(ExceptionMessage.USER_NOT_FOUND_ALARM_Y);
-        }
-
         return userRepository.findAllById(userIds).stream()
                 .filter(User::isPushAlarmYn)
                 .map(User::getId)
