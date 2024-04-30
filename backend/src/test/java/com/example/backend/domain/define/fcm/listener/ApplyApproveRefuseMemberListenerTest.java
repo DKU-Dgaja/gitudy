b445c99ee56602dc845a90f2463042f0b6920a5e
j-ra1n <j_ra1n@naver.com>
2024-04-30 16:18:32 +0900

    [BE] fix(#283): 리스너테스트 수정
    @Mock 주입 -> @Autowired, @MockBean

--- backend/src/test/java/com/example/backend/domain/define/fcm/listener/ApplyApproveRefuseMemberListenerTest.java
+++ backend/src/test/java/com/example/backend/domain/define/fcm/listener/ApplyApproveRefuseMemberListenerTest.java
@@ -3,6 +3,7 @@ package com.example.backend.domain.define.fcm.listener;
 import com.example.backend.auth.TestConfig;
 import com.example.backend.domain.define.event.FcmFixture;
 import com.example.backend.domain.define.fcm.FcmToken;
+import com.example.backend.domain.define.fcm.repository.FcmTokenRepository;
 import com.example.backend.domain.define.study.info.StudyEventFixture;
 import com.example.backend.domain.define.study.info.event.ApplyApproveRefuseMemberEvent;
 import com.example.backend.study.api.event.FcmSingleTokenRequest;
@@ -12,22 +13,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationEventPublisher;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class ApplyApproveRefuseMemberListenerTest extends TestConfig {
 
-    @InjectMocks
+    @Autowired
     private ApplyApproveRefuseMemberListener applyApproveRefuseMemberListener;
 
-    @Mock
+
+    @MockBean
     private FcmService fcmService;
 
-    @Mock
-    private FirebaseMessaging firebaseMessaging;
+    @MockBean
+    private ApplicationEventPublisher applicationEventPublisher;
+
 
     @Test
     @DisplayName("스터디 가입 신청 승인/거부 리스너 테스트")
@@ -39,14 +44,19 @@ public class ApplyApproveRefuseMemberListenerTest extends TestConfig {
 
         FcmToken fcmToken = FcmFixture.generateDefaultFcmToken(applyUserId);
 
-        when(fcmService.findFcmTokenByIdOrThrowException(applyUserId)).thenReturn(fcmToken);
+        when(fcmService.findFcmTokenByIdOrThrowException(any(Long.class))).thenReturn(fcmToken);
 
-        when(firebaseMessaging.send(any())).thenReturn("메시지 전송 완료");
+        applicationEventPublisher.publishEvent(applyApproveRefuseMemberEvent);
 
         // when
         applyApproveRefuseMemberListener.applyApproveRefuseMemberListener(applyApproveRefuseMemberEvent);
 
         // then
-        verify(fcmService).sendMessageSingleDevice(any(FcmSingleTokenRequest.class)); // sendMessageSingleDevice 호출 검증
+
+        /*
+        timeout 을 사용하여 비동기 메서드 호출검증
+        3초내에 sendMessageSingleDevice 가 호출되었는지 검증
+         */
+        verify(fcmService, timeout(3000)).sendMessageSingleDevice(any(FcmSingleTokenRequest.class)); // sendMessageSingleDevice 호출 검증
     }
 }
