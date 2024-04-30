23a1f7064bd4e708b3242c151e430adb4f065925
j-ra1n <j_ra1n@naver.com>
2024-04-30 15:36:26 +0900

    [BE] fix(#283): 불필요한 어노테이션제거

--- backend/src/test/java/com/example/backend/auth/TestConfig.java
+++ backend/src/test/java/com/example/backend/auth/TestConfig.java
@@ -19,16 +19,14 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.event.RecordApplicationEvents;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-@RecordApplicationEvents
-@EnableAsync
+/*@RecordApplicationEvents
+@EnableAsync*/
 public class TestConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer";
