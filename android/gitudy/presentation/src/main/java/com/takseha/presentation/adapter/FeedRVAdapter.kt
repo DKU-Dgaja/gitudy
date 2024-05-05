7ba14f7ecac8e161f86c0a81f1d4712fb20c5185
탁세하 <takseha98@gmail.com>
2024-05-06 08:25:16 +0900

    [AN] feat(#267): feed rvAdapter 배경색 기능, localDateTime 기능 구현

--- android/gitudy/presentation/src/main/java/com/takseha/presentation/adapter/FeedRVAdapter.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/adapter/FeedRVAdapter.kt
@@ -1,6 +1,7 @@
 package com.takseha.presentation.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -10,14 +11,15 @@ import com.takseha.data.dto.feed.StudyPeriod
 import com.takseha.data.dto.mystudy.StudyInfo
 import com.takseha.presentation.R
 import com.takseha.presentation.databinding.ItemFeedBinding
-import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 
 // itemImageView.clipToOutline = true 이거 프로필 이미지 둥글게 할 때 사용
 class FeedRVAdapter(val context : Context, val studyInfoList : List<StudyInfo>) : RecyclerView.Adapter<FeedRVAdapter.ViewHolder>() {
-
+    private val backgroundColorList = listOf("#00BE93", "#00A19A", "#008291", "#08647A", "#386C5F", "#6E9B7B")
 
     class ViewHolder(val binding: ItemFeedBinding) : RecyclerView.ViewHolder(binding.root) {
+        val backgroundColor = binding.studyInfoLayout
         val studyName = binding.studyName
         val commitRule = binding.commitRule
         val teamInfo = binding.teamRankAndRecentInfo
@@ -27,21 +29,22 @@ class FeedRVAdapter(val context : Context, val studyInfoList : List<StudyInfo>) 
         val totalMember = binding.totalCnt
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedRVAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemFeedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return ViewHolder(binding)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    override fun onBindViewHolder(holder: FeedRVAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.backgroundColor.setBackgroundColor(Color.parseColor(backgroundColorList[position % 6]))
         holder.studyName.text = studyInfoList[position].topic
         holder.commitRule.text = setCommitRule(studyInfoList[position].periodType)
         holder.teamInfo.text = context.getString(R.string.study_team_rank_full, position, studyInfoList[position].lastCommitDay)
         holder.teamScore.text = studyInfoList[position].score.toString()
-        holder.totalDayCnt.text = context.getString(R.string.study_total_day_cnt, calculateTotalDayCnt(studyInfoList[position].createdDateTime.toLocalDate()))
+        holder.totalDayCnt.text = context.getString(R.string.study_total_day_cnt, calculateTotalDayCnt(studyInfoList[position].createdDateTime))
         holder.currentMember.text = studyInfoList[position].currentMember.toString()
-        holder.totalMember.text = studyInfoList[position].maximumMember.toString()
+        holder.totalMember.text = context.getString(R.string.study_member_rv, studyInfoList[position].maximumMember)
     }
 
     override fun getItemCount(): Int {
@@ -57,9 +60,12 @@ class FeedRVAdapter(val context : Context, val studyInfoList : List<StudyInfo>) 
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun calculateTotalDayCnt(createdDate: LocalDate): Long {
-        val nowDate = LocalDate.now()
+    private fun calculateTotalDayCnt(createdDate: String): Long {
+        val createdDateLocalDate = LocalDateTime.parse(createdDate)
+        assert(createdDateLocalDate.toString() == createdDate)
 
-        return ChronoUnit.DAYS.between(nowDate, createdDate)
+        val nowDate = LocalDateTime.now()
+
+        return ChronoUnit.DAYS.between(createdDateLocalDate, nowDate)
     }
 }
