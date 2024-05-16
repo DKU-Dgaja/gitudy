426186a9b5a938b4ae28154f42207b8507bb7778
이주성 <32183520@dankook.ac.kr>
2024-05-16 20:47:32 +0900

    [BE] refactor(#312): 커밋 패치 로직 분리

--- backend/src/main/java/com/example/backend/study/api/service/commit/StudyCommitService.java
+++ backend/src/main/java/com/example/backend/study/api/service/commit/StudyCommitService.java
@@ -20,6 +20,7 @@ import com.example.backend.study.api.service.commit.response.CommitInfoResponse;
 import com.example.backend.study.api.service.convention.StudyConventionService;
 import com.example.backend.study.api.service.github.GithubApiService;
 import com.example.backend.study.api.service.github.response.GithubCommitResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -83,23 +84,10 @@ public class StudyCommitService {
             if (commitPage.isEmpty()) break;
 
             // 사용자 조회 로직을 최소화하기 위해 map에 미리 저장
-            List<String> githubIds = commitPage.stream()
-                    .map(GithubCommitResponse::getAuthorName)
-                    .distinct() // 중복 제거
-                    .toList();
-
-            List<User> users = userRepository.findByGithubIdIn(githubIds);
-            Map<String, User> userMap = users.stream()
-                    .collect(Collectors.toMap(User::getGithubId, Function.identity()));
+            Map<String, User> userMap = getUserMap(commitPage);
 
             // 스터디 투두 매핑 조회 로직 최소화를 위해 map에 미리 저장
-            List<Long> userIds = users.stream()
-                    .map(User::getId)
-                    .toList();
-
-            List<StudyTodoMapping> todoMappings = studyTodoMappingRepository.findByTodoIdAndUserIds(todo.getId(), userIds);
-            Map<Long, StudyTodoMapping> todoMappingMap = todoMappings.stream()
-                    .collect(Collectors.toMap(StudyTodoMapping::getUserId, Function.identity()));
+            Map<Long, StudyTodoMapping> todoMappingMap = getTodoMappingMap(todo, userMap);
 
             // StudyCommit으로 저장되지 않은 것만 필터링
             List<GithubCommitResponse> unsavedCommits = studyCommitRepository.findUnsavedGithubCommits(commitPage);
@@ -107,34 +95,8 @@ public class StudyCommitService {
             // 검증할 컨벤션 조회
             StudyConventionResponse conventions = studyConventionRepository.findActiveConventionByStudyInId(study.getId());
 
-            List<StudyCommit> commitList = unsavedCommits.stream()
-                    .filter(commit -> {
-                        User user = userMap.get(commit.getAuthorName());
-
-                        return user != null &&
-                                // 활동중인 스터디 멤버인지
-                                studyMemberMap.containsKey(user.getId()) &&
-                                // 컨벤션을 지켰는지
-                                studyConventionService.checkConvention(conventions.getContent(), commit.getMessage());
-                    })
-                    .map(commit -> {
-                        User findUser = userMap.get(commit.getAuthorName());
-                        StudyTodoMapping todoMapping = todoMappingMap.get(findUser.getId());
-
-                        // 마감일이 지났는지 확인
-                        if (todoMapping != null && commit.getCommitDate().isAfter(todo.getTodoDate())) {
-                            // 마감일이 지났다면 상태를 TODO_OVERDUE로 변경
-                            todoMapping.updateTodoMappingStatus(StudyTodoStatus.TODO_OVERDUE);
-                        } else {
-                            // 마감일이 지나지 않았다면 상태를 TODO_COMPLETE로 변경
-                            if (todoMapping != null) {
-                                todoMapping.updateTodoMappingStatus(StudyTodoStatus.TODO_COMPLETE);
-                            }
-                        }
-
-                        return StudyCommit.of(findUser.getId(), todo, commit, CommitStatus.COMMIT_APPROVAL);
-                    })
-                    .toList();
+            // 저장할 커밋 필더링
+            List<StudyCommit> commitList = filterCommit(todo, unsavedCommits, userMap, studyMemberMap, conventions, todoMappingMap);
 
             studyCommitRepository.saveAll(commitList);
 
@@ -146,6 +108,62 @@ public class StudyCommitService {
         }
 
         log.info(">>>> [ 원격 레포지토리로부터 커밋 업데이트를 성공하였습니다. ] <<<<");
+    }
+
+    private List<StudyCommit> filterCommit(StudyTodo todo, List<GithubCommitResponse> unsavedCommits,
+                                           Map<String, User> userMap, Map<Long, StudyMember> studyMemberMap,
+                                           StudyConventionResponse conventions, Map<Long, StudyTodoMapping> todoMappingMap) {
+
+        return unsavedCommits.stream()
+                .filter(commit -> {
+                    User user = userMap.get(commit.getAuthorName());
+
+                    return user != null &&
+                            // 활동중인 스터디 멤버인지
+                            studyMemberMap.containsKey(user.getId()) &&
+                            // 컨벤션을 지켰는지
+                            studyConventionService.checkConvention(conventions.getContent(), commit.getMessage());
+                })
+                .map(commit -> {
+                    User findUser = userMap.get(commit.getAuthorName());
+                    StudyTodoMapping todoMapping = todoMappingMap.get(findUser.getId());
+
+                    // 마감일이 지났는지 확인
+                    if (todoMapping != null && commit.getCommitDate().isAfter(todo.getTodoDate())) {
+                        // 마감일이 지났다면 상태를 TODO_OVERDUE로 변경
+                        todoMapping.updateTodoMappingStatus(StudyTodoStatus.TODO_OVERDUE);
+                    } else {
+                        // 마감일이 지나지 않았다면 상태를 TODO_COMPLETE로 변경
+                        if (todoMapping != null) {
+                            todoMapping.updateTodoMappingStatus(StudyTodoStatus.TODO_COMPLETE);
+                        }
+                    }
+
+                    return StudyCommit.of(findUser.getId(), todo, commit, CommitStatus.COMMIT_APPROVAL);
+                })
+                .toList();
+
+    }
+
+    private Map<String, User> getUserMap(List<GithubCommitResponse> commitPage) {
+        List<String> githubIds = commitPage.stream()
+                .map(GithubCommitResponse::getAuthorName)
+                .distinct() // 중복 제거
+                .toList();
+
+        List<User> users = userRepository.findByGithubIdIn(githubIds);
+        return users.stream()
+                .collect(Collectors.toMap(User::getGithubId, Function.identity()));
+    }
+
+    private Map<Long, StudyTodoMapping> getTodoMappingMap(StudyTodo todo, Map<String, User> userMap) {
+        List<Long> userIds = userMap.values().stream()
+                .map(User::getId)
+                .toList();
+
+        List<StudyTodoMapping> todoMappings = studyTodoMappingRepository.findByTodoIdAndUserIds(todo.getId(), userIds);
+        return todoMappings.stream()
+                .collect(Collectors.toMap(StudyTodoMapping::getUserId, Function.identity()));
     }
 
     @Transactional
