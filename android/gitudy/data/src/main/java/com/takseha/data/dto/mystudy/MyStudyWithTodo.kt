f76e5576f6c846e8c06a7ccc33025b4f505d0d0a
탁세하 <takseha98@gmail.com>
2024-05-19 09:40:10 +0900

    [AN] refactor(#299): MyStudyWithTodo data class 수정

--- android/gitudy/data/src/main/java/com/takseha/data/dto/mystudy/MyStudyWithTodo.kt
+++ android/gitudy/data/src/main/java/com/takseha/data/dto/mystudy/MyStudyWithTodo.kt
@@ -1,23 +1,12 @@
 package com.takseha.data.dto.mystudy
 
-import com.takseha.data.dto.feed.StudyPeriod
+import com.takseha.data.dto.feed.StudyInfo
 
 data class MyStudyWithTodo(
-    val createdDateTime: String,
-    val currentMember: Int,
-    val id: Int,
-    val info: String,
-    val lastCommitDay: String,
-    val maximumMember: Int,
-    val periodType: StudyPeriod,
-    val profileImageUrl: String,
-    val score: Int,
-    val topic: String,
-    val userId: Int,
+    val studyInfo: StudyInfo,
     val todoTitle: String,
     val todoTime: String,
     val todoCheck: String, // enum
-    val totalNum: Int,
     val todoCheckNum: Int
 )
 
