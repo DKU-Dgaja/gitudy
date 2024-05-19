2f4c11a3f4dd41a19cc34b1f3db2f1ec70e202c9
탁세하 <takseha98@gmail.com>
2024-05-20 01:36:56 +0900

    [AN] feat(#318): 스터디 메인 페이지 초기 세팅 및 studyInfoId 수신 로직 구현

--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/mystudy/MyStudyMainActivity.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/mystudy/MyStudyMainActivity.kt
@@ -1,12 +1,32 @@
 package com.takseha.presentation.ui.mystudy
 
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.takseha.presentation.R
+import com.takseha.presentation.databinding.ActivityMyStudyMainBinding
 
 class MyStudyMainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMyStudyMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_study_main)
+        setBinding()
+        // todo: statusBar 색 변경
+
+        val studyInfoId = intent.getIntExtra("studyInfoId", 0)
+        Log.d("MyStudyMainActivity", studyInfoId.toString())
+
+        with(binding) {
+
+        }
+    }
+
+    private fun setBinding() {
+        binding = ActivityMyStudyMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
