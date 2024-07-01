7d20b0f6c6df2e4ff3ccfeecc3f9fdf240f57f0b
kooyeongmin <rn9dud0@naver.com>
2024-07-01 18:16:26 +0900

    [BE] refactor(#372): DTO with UserInfo생성

--- backend/src/main/java/com/example/backend/study/api/controller/info/response/StudyInfoListAndCursorIdxResponse.java
+++ backend/src/main/java/com/example/backend/study/api/controller/info/response/StudyInfoListAndCursorIdxResponse.java
@@ -9,21 +9,18 @@ import java.util.Map;
 
 @Getter
 public class StudyInfoListAndCursorIdxResponse {
-    private List<StudyInfoListResponse> studyInfoList;
+    private List<StudyInfoListWithMemberResponse> studyInfoList;
 
     private Long cursorIdx;
 
-    // Map<STUDY_INFO_ID, List<UserNameAndProfileImageResponse>>
-    Map<Long, List<UserNameAndProfileImageResponse>> studyUserInfoMap;
 
     // Map<STUDY_INFO_ID, List<STUDY_CATEGORY_NAME>>
     private Map<Long, List<String>> studyCategoryMappingMap;
 
     @Builder
-    public StudyInfoListAndCursorIdxResponse(List<StudyInfoListResponse> studyInfoList, Long cursorIdx, Map<Long, List<UserNameAndProfileImageResponse>> studyUserInfoMap, Map<Long, List<String>> studyCategoryMappingMap) {
+    public StudyInfoListAndCursorIdxResponse(List<StudyInfoListWithMemberResponse> studyInfoList, Long cursorIdx, Map<Long, List<String>> studyCategoryMappingMap) {
         this.studyInfoList = studyInfoList;
         this.cursorIdx = cursorIdx;
-        this.studyUserInfoMap = studyUserInfoMap;
         this.studyCategoryMappingMap = studyCategoryMappingMap;
     }
 
