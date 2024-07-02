16eb665b0428056308798647d2a8316fccf383ba
탁세하 <takseha98@gmail.com>
2024-07-02 16:34:14 +0900

    [AN] feat(#377): 토큰 인증 및 재발급 로직 관련 RetrofitInstance 구현

--- android/gitudy/app/src/main/java/com/takseha/gitudy/GitudyApplication.kt
+++ android/gitudy/app/src/main/java/com/takseha/gitudy/GitudyApplication.kt
@@ -1,11 +1,13 @@
 package com.takseha.gitudy
 
 import android.app.Application
+import com.takseha.data.api.gitudy.RetrofitInstance
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GitudyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        RetrofitInstance.init(this)
     }
 }
--- android/gitudy/data/src/main/java/com/takseha/data/api/gitudy/RetrofitInstance.kt
+++ android/gitudy/data/src/main/java/com/takseha/data/api/gitudy/RetrofitInstance.kt
@@ -1,17 +1,35 @@
 package com.takseha.data.api.gitudy
 
+import android.content.Context
 import com.takseha.data.BuildConfig
+import com.takseha.data.TokenInterceptor
+import com.takseha.data.TokenManager
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private val client: Retrofit = Retrofit
-        .Builder()
-        .baseUrl(BuildConfig.GITUDY_BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    private lateinit var tokenManager: TokenManager
 
-    fun getInstance() : Retrofit {
-        return client
+    fun init(context: Context) {
+        tokenManager = TokenManager(context)
+    }
+
+    private val okHttpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(TokenInterceptor(tokenManager))
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.GITUDY_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun getInstance(): Retrofit {
+        return retrofit
     }
 }
