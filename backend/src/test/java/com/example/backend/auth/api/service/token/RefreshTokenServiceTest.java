d017789bd8d49f6674550e7b611c1d281e688c77
j-ra1n <j_ra1n@naver.com>
2024-07-13 09:48:13 +0900

    [BE] test(#386): test 수정

--- backend/src/test/java/com/example/backend/auth/api/service/token/RefreshTokenServiceTest.java
+++ backend/src/test/java/com/example/backend/auth/api/service/token/RefreshTokenServiceTest.java
@@ -1,18 +1,17 @@
 package com.example.backend.auth.api.service.token;
 
+import com.example.backend.TestConfig;
 import com.example.backend.auth.api.service.jwt.JwtService;
 import com.example.backend.common.exception.ExceptionMessage;
 import com.example.backend.common.exception.jwt.JwtException;
-import com.example.backend.domain.define.refreshToken.RefreshToken;
-import com.example.backend.domain.define.refreshToken.repository.RefreshTokenRepository;
 import com.example.backend.domain.define.account.user.User;
 import com.example.backend.domain.define.account.user.constant.UserRole;
-import com.example.backend.domain.define.account.user.constant.UserPlatformType;
 import com.example.backend.domain.define.account.user.repository.UserRepository;
+import com.example.backend.domain.define.refreshToken.RefreshToken;
+import com.example.backend.domain.define.refreshToken.repository.RefreshTokenRepository;
 import io.jsonwebtoken.Claims;
-import com.example.backend.TestConfig;
-
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +19,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.HashMap;
 import java.util.Optional;
 
+import static com.example.backend.domain.define.account.user.constant.UserPlatformType.KAKAO;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class RefreshTokenServiceTest extends TestConfig {
     @Autowired
@@ -35,15 +36,22 @@ class RefreshTokenServiceTest extends TestConfig {
 
     @Autowired
     private UserRepository userRepository;
+
     @AfterEach
     void tearDown() {
         userRepository.deleteAllInBatch();
     }
+
+    @BeforeEach
+    void setUp() {
+        refreshTokenRepository.deleteAll();
+    }
+
     @Test
     @DisplayName("Refresh Token 저장 & 조회 테스트")
     void redisRefreshTokenGenerate() {
         // given
-        String testRefreshToken="testToken";
+        String testRefreshToken = "testToken";
 
         String testSubject = "KAKAO_1234";
 
@@ -63,6 +71,7 @@ class RefreshTokenServiceTest extends TestConfig {
 
         assertThat(testToken.get().getSubject()).isEqualTo(testSubject);
     }
+
     @Test
     @DisplayName("Redis에 존재하지 않는 Refresh Token으로 요청할 경우 JWT_NOT_EXIST_RTK 예외가 발생한다.")
     void ReissueWhenRedisNotExistRefreshTokenTest() {
@@ -70,7 +79,7 @@ class RefreshTokenServiceTest extends TestConfig {
         User user = User.builder()
                 .name("구영민")
                 .role(UserRole.USER)
-                .platformType(UserPlatformType.KAKAO)
+                .platformType(KAKAO)
                 .platformId("1234")
                 .profileImageUrl("https://kakao.com")
                 .build();
@@ -134,5 +143,39 @@ class RefreshTokenServiceTest extends TestConfig {
         // then
         Optional<RefreshToken> deletedToken = refreshTokenRepository.findBySubject(saveToken.getSubject());
         assertFalse(deletedToken.isPresent());
+    }
+
+    @Test
+    void 이미_토큰이_존재하면_예외처리_하지않고_로그() {
+        // given
+        User user = User.builder()
+                .name("이정우")
+                .role(UserRole.USER)
+                .platformType(KAKAO)
+                .platformId("1234")
+                .profileImageUrl("https://kakao.com")
+                .build();
+        User savedUser = userRepository.save(user);
+
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("role", savedUser.getRole().name());
+        map.put("platformId", savedUser.getPlatformId());
+        map.put("platformType", String.valueOf(savedUser.getPlatformType()));
+
+        String accessToken = jwtService.generateAccessToken(map, savedUser);
+
+
+        // when
+        String sub = jwtService.extractAllClaims(accessToken).getSubject();
+
+        Optional<RefreshToken> rtk = refreshTokenRepository.findBySubject(sub);
+
+        rtk.ifPresentOrElse(
+                refreshToken -> {
+
+                },
+                () -> System.out.print("이미_토큰이_존재하면_예외처리_하지않고_로그")
+        );
     }
 }
