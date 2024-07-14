7efd3db8c80dde94beb628977ba4703c4f528d4b
이주성 <32183520@dankook.ac.kr>
2024-07-14 17:55:09 +0900

    [BE] refactor(#405): 에러 시 ErrorResponse를 바디로 반환하도록 수정

--- backend/src/main/java/com/example/backend/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/backend/common/exception/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.example.backend.common.exception;
 
 import com.example.backend.common.exception.jwt.JwtException;
-import com.example.backend.common.response.JsonResult;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +9,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.stream.Collectors;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,14 +19,14 @@ public class GlobalExceptionHandler {
         데이터 바인딩 중 발생하는 에러 BindException 처리
      */
     @ExceptionHandler(BindException.class)
-    public ResponseEntity<JsonResult> bindException(BindException e) {
-        JsonResult error = JsonResult.failOf(
-                e.getBindingResult()
-                        .getFieldErrors()
-                        .stream()
-                        .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
-                        .collect(Collectors.joining(", "))
-        );
+    public ResponseEntity<ErrorResponse> bindException(BindException e) {
+        String errorMsg = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorResponse error = ErrorResponse.from(BAD_REQUEST.value(), BAD_REQUEST.getReasonPhrase(), errorMsg);
 
         return ResponseEntity.badRequest().body(error);
     }
@@ -34,14 +35,14 @@ public class GlobalExceptionHandler {
         @Valid 어노테이션을 사용한 DTO의 유효성 검사에서 예외가 발생한 경우
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<JsonResult> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
-        JsonResult error = JsonResult.failOf(
-                e.getBindingResult()
-                        .getFieldErrors()
-                        .stream()
-                        .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
-                        .collect(Collectors.joining(", "))
-        );
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        String errorMsg = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorResponse error = ErrorResponse.from(BAD_REQUEST.value(), BAD_REQUEST.getReasonPhrase(), errorMsg);
 
         return ResponseEntity.badRequest().body(error);
     }
@@ -50,13 +51,16 @@ public class GlobalExceptionHandler {
         JWT 관련 예외 처리
      */
     @ExceptionHandler(com.example.backend.common.exception.jwt.JwtException.class)
-    public ResponseEntity<JsonResult> handleJwtException(JwtException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(JsonResult.failOf(e.getMessage()));
+    public ResponseEntity<ErrorResponse> handleJwtException(JwtException e) {
+
+        return ResponseEntity.status(UNAUTHORIZED)
+                .body(ErrorResponse.from(UNAUTHORIZED.value(), UNAUTHORIZED.getReasonPhrase(), e.getMessage()));
 
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<JsonResult> exception(Exception e) {
-        return ResponseEntity.badRequest().body(JsonResult.failOf(e.getMessage()));
+    public ResponseEntity<ErrorResponse> exception(Exception e) {
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.from(BAD_REQUEST.value(), BAD_REQUEST.getReasonPhrase(), e.getMessage()));
     }
 }
