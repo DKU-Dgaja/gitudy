7ea13d8ba4d95cf103d684aafce13a99ee9d1779
탁세하 <takseha98@gmail.com>
2024-07-22 15:21:21 +0900

    [AN] feat(#434): android studio 및 gradle version update

--- android/gitudy/build.gradle.kts
+++ android/gitudy/build.gradle.kts
@@ -9,9 +9,9 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.android.library") version "8.2.0" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("com.android.library") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.10" apply false
 
     id("com.google.dagger.hilt.android") version "2.44" apply false
