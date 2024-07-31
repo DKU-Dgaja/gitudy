a67efac222d16de12f758a3527661d4f6a008510
탁세하 <takseha98@gmail.com>
2024-07-31 14:48:59 +0900

    [AN] refactor(#464): 회원가입 시 request에 fcm 토큰 추가

--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/auth/InputIdFragment.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/auth/InputIdFragment.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.takseha.presentation.R
 import com.takseha.presentation.databinding.FragmentInputIdBinding
+import com.takseha.presentation.firebase.MyFirebaseMessagingService
 import com.takseha.presentation.viewmodel.auth.RegisterViewModel
 import kotlinx.coroutines.launch
 
@@ -90,11 +91,17 @@ class InputIdFragment : Fragment() {
             }
 
             confirmBtn.setOnClickListener { view ->
-                viewModel.setGithubId(inputIdEditText.text.toString())
-                viewModel.getRegisterTokens()
-                Log.d("InputIdFragment", viewModel.registerInfoState.value.toString())
-                view.findNavController()
-                    .navigate(R.id.action_inputIdFragment_to_loginCompleteFragment)
+                viewLifecycleOwner.lifecycleScope.launch {
+                    val fcmToken = MyFirebaseMessagingService.getFirebaseToken().toString()
+                    viewModel.apply {
+                        setGithubId(inputIdEditText.text.toString())
+                        setFCMToken(fcmToken)
+                        getRegisterTokens()
+                    }
+                    Log.d("InputIdFragment", viewModel.registerInfoState.value.toString())
+                    view.findNavController()
+                        .navigate(R.id.action_inputIdFragment_to_loginCompleteFragment)
+                }
             }
         }
     }
