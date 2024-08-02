8c07adf46557dcbfacd23e50694f689e445923c5
탁세하 <takseha98@gmail.com>
2024-08-03 07:40:55 +0900

    [AN] feat(#464): 스터디 comment 상세 화면 내 스터디 Comment 수정/삭제 기능 구현

--- android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/mystudy/StudyCommentBoardActivity.kt
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/mystudy/StudyCommentBoardActivity.kt
@@ -6,10 +6,12 @@ import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
@@ -19,6 +21,7 @@ import com.takseha.presentation.R
 import com.takseha.presentation.adapter.CommentListRVAdapter
 import com.takseha.presentation.databinding.ActivityMyStudyMainBinding
 import com.takseha.presentation.databinding.ActivityStudyCommentBoardBinding
+import com.takseha.presentation.ui.common.CustomDialog
 import com.takseha.presentation.ui.home.MainHomeActivity
 import com.takseha.presentation.viewmodel.mystudy.MyStudyMainViewModel
 import kotlinx.coroutines.flow.collectLatest
@@ -33,6 +36,7 @@ class StudyCommentBoardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_study_comment_board)
         setBinding()
+        window.statusBarColor = ContextCompat.getColor(this, R.color.BACKGROUND)
 
         val studyInfoId = intent.getIntExtra("studyInfoId", 0)
         var comment = ""
@@ -87,7 +91,67 @@ class StudyCommentBoardActivity : AppCompatActivity() {
             val commentListRVAdapter = CommentListRVAdapter(this@StudyCommentBoardActivity, comments)
             commentList.adapter = commentListRVAdapter
             commentList.layoutManager = LinearLayoutManager(this@StudyCommentBoardActivity)
+
+            clickStudyCommentItem(commentListRVAdapter, comments)
         }
+    }
+
+    // TODO: 수정 시 메세지 입력 창 같이 떠오르는 현상 없애고, edittext가 자판 위로 오도록 처리
+    private fun clickStudyCommentItem(commentListRVAdapter: CommentListRVAdapter, commentList: List<StudyComment>) {
+        commentListRVAdapter.onClickListener = object : CommentListRVAdapter.OnClickListener {
+            override fun onUpdateClick(view: View, position: Int) {
+                val commentViewHolder = binding.commentList.findViewHolderForAdapterPosition(position) as CommentListRVAdapter.ViewHolder
+                var contentBody = commentViewHolder.binding.contentEditText
+                var confirmBtn = commentViewHolder.binding.confirmBtn
+                var content: String
+
+                contentBody.addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+
+                    override fun afterTextChanged(s: Editable?) {
+                        content = contentBody.text.toString()
+                        confirmBtn.isEnabled = content.isNotEmpty()
+
+                        if (confirmBtn.isEnabled) confirmBtn.setTextColor(ContextCompat.getColor(this@StudyCommentBoardActivity, R.color.GS_700))
+                        else confirmBtn.setTextColor(ContextCompat.getColor(this@StudyCommentBoardActivity, R.color.GS_400))
+                    }
+                })
+            }
+
+            override fun onConfirmClick(view: View, position: Int) {
+                val commentViewHolder = binding.commentList.findViewHolderForAdapterPosition(position) as CommentListRVAdapter.ViewHolder
+                var content = commentViewHolder.binding.contentEditText.text.toString()
+
+                showUpdateCommentDialog(commentList[position].studyInfoId, commentList[position].id, content)
+            }
+
+            override fun onDeleteClick(view: View, position: Int) {
+                showDeleteCommentDialog(commentList[position].studyInfoId, commentList[position].id)
+            }
+        }
+    }
+
+    private fun showUpdateCommentDialog(studyInfoId: Int, studyCommentId: Int, content: String) {
+        val customDialog = CustomDialog(this)
+        customDialog.setAlertText(getString(R.string.study_comment_update))
+        customDialog.setOnConfirmClickListener {
+            viewModel.updateStudyComment(studyInfoId, studyCommentId, content, 10)
+        }
+        customDialog.setOnCancelClickListener {
+            viewModel.getStudyComments(studyInfoId, 10)
+        }
+        customDialog.show()
+    }
+
+    private fun showDeleteCommentDialog(studyInfoId: Int, studyCommentId: Int) {
+        val customDialog = CustomDialog(this)
+        customDialog.setAlertText(getString(R.string.study_comment_delete))
+        customDialog.setOnConfirmClickListener {
+            viewModel.deleteStudyComment(studyInfoId, studyCommentId, 10)
+        }
+        customDialog.show()
     }
 
     private fun setBinding() {
