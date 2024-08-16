3cf97ecdf4513aa50f10623740706d795aa9c67f
탁세하 <takseha98@gmail.com>
2024-08-16 13:39:15 +0900

    [AN] refactor(#525): 북마크 관련 api 호출 관련 기능 구현

---
+++ android/gitudy/data/src/main/java/com/takseha/data/repository/gitudy/GitudyBookmarksRepository.kt
@@ -0,0 +1,19 @@
+package com.takseha.data.repository.gitudy
+
+import com.takseha.data.api.gitudy.GitudyBookmarksService
+import com.takseha.data.api.gitudy.RetrofitInstance
+import com.takseha.data.api.gitudy.GitudyMemberService
+import com.takseha.data.dto.feed.MessageRequest
+
+class GitudyBookmarksRepository {
+    private val client = RetrofitInstance.getInstance().create(GitudyBookmarksService::class.java)
+
+    suspend fun getBookmarks(
+        cursorIdx: Long?,
+        limit: Long
+    ) = client.getBookmarks(cursorIdx, limit)
+
+    suspend fun setBookmarkStatus(
+        studyInfoId: Int
+    ) = client.setBookmarkStatus(studyInfoId)
+}
