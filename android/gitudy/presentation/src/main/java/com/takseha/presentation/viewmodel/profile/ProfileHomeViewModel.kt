1ad5ce435af5797f2fe290a363fb364853f9e2b5
탁세하 <takseha98@gmail.com>
2024-08-17 23:36:10 +0900

    [AN] feat(#528): 프로필 홈 화면 관련 기능 구현

---
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/viewmodel/profile/ProfileHomeViewModel.kt
@@ -0,0 +1,73 @@
+package com.takseha.presentation.viewmodel.profile
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.takseha.data.dto.auth.auth.UserInfoUpdatePageResponse
+import com.takseha.data.dto.feed.StudyCountResponse
+import com.takseha.data.dto.feed.StudyInfo
+import com.takseha.data.dto.mystudy.SocialInfo
+import com.takseha.data.dto.mystudy.TodoProgressResponse
+import com.takseha.data.repository.gitudy.GitudyAuthRepository
+import com.takseha.data.repository.gitudy.GitudyStudyRepository
+import com.takseha.presentation.R
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.io.Serializable
+
+class ProfileHomeViewModel : ViewModel() {
+    private var gitudyAuthRepository = GitudyAuthRepository()
+
+    private val _uiState = MutableStateFlow(ProfileInfoUiState())
+    val uiState = _uiState.asStateFlow()
+
+    suspend fun getUserProfileInfo() {
+        val userProfileInfoResponse = gitudyAuthRepository.getUserInfoUpdatePage()
+
+        if (userProfileInfoResponse.isSuccessful) {
+            val userProfileInfo = userProfileInfoResponse.body()!!
+            val userGithubId = getUserGithubId()
+            _uiState.update {
+                it.copy(
+                    name = userProfileInfo.name,
+                    githubId = userGithubId,
+                    profileImgUrl = userProfileInfo.profileImageUrl,
+                    profilePublicYn = userProfileInfo.profilePublicYn,
+                    socialInfo = userProfileInfo.socialInfo
+                )
+            }
+        } else {
+            Log.e(
+                "ProfileHomeViewModel",
+                "userProfileInfoResponse status: ${userProfileInfoResponse.code()}\nuserProfileInfoResponse message: ${userProfileInfoResponse.errorBody()?.string()}"
+            )
+        }
+    }
+
+    private suspend fun getUserGithubId(): String {
+        val userGithubIdResponse = gitudyAuthRepository.getUserInfo()
+
+        if (userGithubIdResponse.isSuccessful) {
+            return userGithubIdResponse.body()!!.githubId
+        } else {
+            Log.e(
+                "ProfileHomeViewModel",
+                "userGithubIdResponse status: ${userGithubIdResponse.code()}\nuserGithubIdResponse message: ${userGithubIdResponse.errorBody()?.string()}"
+            )
+        }
+        return "noGithubId"
+    }
+}
+
+data class ProfileInfoUiState(
+    val name: String = "",
+    val githubId: String = "",
+    val profileImgUrl: String = "",
+    val profilePublicYn: Boolean? = null,
+    val socialInfo: SocialInfo? = null
+)
