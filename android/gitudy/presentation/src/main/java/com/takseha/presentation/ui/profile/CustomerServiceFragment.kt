7b8b3dc0a371b519aa2a9fb655073e2b0592b7bd
탁세하 <takseha98@gmail.com>
2024-08-20 17:41:42 +0900

    [AN] feat(#532): 회원 탈퇴, 로그아웃 로직 완성

---
+++ android/gitudy/presentation/src/main/java/com/takseha/presentation/ui/profile/CustomerServiceFragment.kt
@@ -0,0 +1,36 @@
+package com.takseha.presentation.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.takseha.presentation.databinding.FragmentCustomerServiceBinding
+
+class CustomerServiceFragment : Fragment() {
+    private var _binding: FragmentCustomerServiceBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCustomerServiceBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.backBtn.setOnClickListener { it.findNavController().popBackStack() }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
