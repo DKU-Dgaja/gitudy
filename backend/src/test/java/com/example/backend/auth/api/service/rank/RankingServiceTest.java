d508640bb8b9579394dc66a98d73f5f7f335589d
j-ra1n <j_ra1n@naver.com>
2024-09-07 00:09:54 +0900

    [BE] test(#609): test

--- backend/src/test/java/com/example/backend/auth/api/service/rank/RankingServiceTest.java
+++ backend/src/test/java/com/example/backend/auth/api/service/rank/RankingServiceTest.java
@@ -28,7 +28,7 @@ public class RankingServiceTest extends MockTestConfig {
     private ZSetOperations<String, Object> zSetOperations;
 
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    private RedisTemplate<String, Object> redisTemplateDb1;
 
     @Autowired
     private StudyInfoRepository studyInfoRepository;
@@ -42,14 +42,14 @@ public class RankingServiceTest extends MockTestConfig {
 
     @BeforeEach
     void setUp() {
-        //   when(redisTemplate.opsForZSet()).thenReturn(zSetOperations);
+        //   when(redisTemplateDb1.opsForZSet()).thenReturn(zSetOperations);
     }
 
     @AfterEach
     void tearDown() {
         userRepository.deleteAllInBatch();
-        redisTemplate.delete(USER_RANKING_KEY);
-        redisTemplate.delete(STUDY_RANKING_KEY);
+        redisTemplateDb1.delete(USER_RANKING_KEY);
+        redisTemplateDb1.delete(STUDY_RANKING_KEY);
     }
 
     @Test
