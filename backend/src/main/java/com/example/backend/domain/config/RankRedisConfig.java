5c782d3ad70b2bb5ae79c8452b8956f30a20a416
j-ra1n <j_ra1n@naver.com>
2024-09-07 00:09:43 +0900

    [BE] feat(#609): 레디스 데이터 파티셔닝

--- backend/src/main/java/com/example/backend/domain/config/RankRedisConfig.java
+++ backend/src/main/java/com/example/backend/domain/config/RankRedisConfig.java
@@ -1,18 +1,18 @@
 package com.example.backend.domain.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories
-public class RedisConfig {
+public class RankRedisConfig {
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -20,20 +20,25 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    // Bean으로 등록해 Redis 연결 - Lettuce 사용
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    public RedisConnectionFactory redisConnectionFactoryDb1() {
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(host, port);
+        factory.setDatabase(1);  // DB1 사용
+        return factory;
     }
 
-    // Sorted Set 사용하기 위한 Template, Operations
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplateDb1() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactoryDb1());
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 
+    @Bean
+    public ZSetOperations<String, Object> zSetOperations(@Qualifier("redisTemplateDb1") RedisTemplate<String, Object> redisTemplateDb1) {
+        return redisTemplateDb1.opsForZSet();
+    }
 }
